24d1aa9505da85d76df4d34fa41d28b47fa50b83
Roman Dementiev <dementiev@ira.uka.de>
2005-05-18 07:37:07 +0000

    testmap.cpp added

---
+++ containers/testmap.cpp
@@ -0,0 +1,69 @@
+/***************************************************************************
+ *            testmap.cpp
+ *
+ *  Tue Mar 22 10:41:38 2005
+ *  Copyright  2005  Roman Dementiev
+ *  Email dementiev@ira.uka.de
+ ****************************************************************************/
+#include "map.h"
+#include <algorithm>
+#include <math.h>
+
+typedef unsigned int key_type;
+typedef unsigned int data_type;
+
+struct cmp: public std::less<key_type>
+{
+                key_type min_value() const { return std::numeric_limits<key_type>::min(); };
+                key_type max_value() const { return std::numeric_limits<key_type>::max(); };
+};
+
+#define BLOCK_SIZE (128*1024)
+#define CACHE_SIZE (128*1024/BLOCK_SIZE)
+
+#define CACHE_ELEMENTS (BLOCK_SIZE*CACHE_SIZE/(sizeof(key_type) + sizeof(data_type)))
+
+typedef stxxl::map<key_type, data_type , cmp, BLOCK_SIZE, CACHE_SIZE > map_type;
+
+int main()
+{
+	stxxl::stats * bm = stxxl::stats::get_instance();
+	STXXL_MSG(*bm)
+
+	STXXL_MSG("Block size "<<BLOCK_SIZE/1024<<" kb")
+	STXXL_MSG("Cache size "<<(CACHE_SIZE*BLOCK_SIZE)/1024<<" kb")
+	for(int mult=1; mult < 256; mult *= 2 )
+	{
+		const unsigned el = mult*(CACHE_ELEMENTS/8);
+		STXXL_MSG("Elements to insert "<< el <<" volume ="<<
+			(el*(sizeof(key_type) + sizeof(data_type)))/1024<<" kb")
+		map_type  Map;
+		for(unsigned i = 0; i < el; ++i)
+		{
+			Map[i] = i+1;
+		}
+		double writes = double(bm->get_writes())/double(el);
+		double logel = log(el)/log(BLOCK_SIZE);
+		STXXL_MSG("Logs: writes "<<writes<<" logel "<<logel<<" writes/logel "<<(writes/logel) )
+		STXXL_MSG(*bm)
+		bm->reset();
+		STXXL_MSG("Doing search")
+		unsigned queries = el;
+		const map_type & ConstMap = Map;
+		for(unsigned i = 0; i< queries ; ++i)
+		{
+			key_type key = random()%el;
+			map_type::const_iterator result = ConstMap.find(key);
+			assert((*result).second == key + 1);
+		}
+		double reads = double(bm->get_reads())/logel;
+	    double readsperq = double(bm->get_reads())/queries;
+		STXXL_MSG("reads/logel "<<reads<<" readsperq "<<readsperq)
+		STXXL_MSG(*bm)
+		bm->reset();
+	}
+	
+	
+	
+	return 0;	
+}
