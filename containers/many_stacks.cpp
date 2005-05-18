ee671f5365fc21f82a74c13337d7aa255a7cd02c
Roman Dementiev <dementiev@ira.uka.de>
2005-05-18 07:37:59 +0000

    many_stacks.cpp added

---
+++ containers/many_stacks.cpp
@@ -0,0 +1,42 @@
+ /***************************************************************************
+ *            test_stack.cpp
+ *
+ *  Thu May  1 22:02:00 2003
+ *  Copyright  2003  Roman Dementiev
+ *  dementiev@mpi-sb.mpg.de
+ ****************************************************************************/
+
+#include "stack.h"
+
+//! \example test_stack.cpp
+//! This is an example of how to use \c stxxl::STACK_GENERATOR class
+//! to generate an \b external stack type 
+//! with \c stxxl::grow_shrink_stack implementation, \b four blocks per page,
+//! block size \b 4096 bytes
+
+using namespace stxxl;
+
+int main(int argc, char * argv[])
+{
+  typedef STACK_GENERATOR<int,external,normal,4,4096>::result ext_stack_type;
+  
+  if(argc<2)
+  {
+    STXXL_MSG("Usage: "<<argv[0]<<" number_of_stacks")
+    abort();
+  }
+
+  char dum;
+  
+  std::cin >> dum;
+  
+  ext_stack_type * my_stacks = new ext_stack_type[atoi(argv[1])];
+ 
+  std::cin >> dum;
+
+  delete [] my_stacks;
+  
+  std::cin >> dum;
+  
+  return 0;
+}
