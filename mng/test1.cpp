099a3ee9c767809a7b69b6e86277fc6d70858623
Roman Dementiev <dementiev@ira.uka.de>
2006-01-30 16:29:41 +0000

    a new test added
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@37 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ mng/test1.cpp
@@ -0,0 +1,26 @@
+#include <stxxl>
+
+
+int main()
+{
+	typedef stxxl::typed_block<1024*1024, double> block_type;
+	std::vector<block_type::bid_type> bids;
+	std::vector<stxxl::request_ptr> requests;
+	stxxl::block_manager* bm = stxxl::block_manager::get_instance();
+	bm->new_blocks<block_type>(32, stxxl::striping(), std::back_inserter(bids));
+	std::vector<block_type, stxxl::new_alloc<block_type> > blocks(32);
+	int vIndex;
+	for (vIndex = 0; vIndex < 32; ++vIndex) {
+		for (int vIndex2 = 0; vIndex2 < block_type::size; ++vIndex2) {
+		blocks[vIndex][vIndex2] = vIndex2;
+		}
+	}
+	for (vIndex = 0; vIndex < 32; ++vIndex) {
+		requests.push_back(blocks[vIndex].write(bids[vIndex]));
+	}
+	stxxl::wait_all(requests.begin(), requests.end());
+	bm->delete_blocks(bids.begin(), bids.end()); 
+	return 1;
+}
+
+
