c651cb470363c02552a4e0dfdb635d41e985ae44
Roman Dementiev <dementiev@ira.uka.de>
2006-07-10 14:39:04 +0000

    a skeleton for pq benchmark added
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@143 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ containers/pq_benchmark.cpp
@@ -0,0 +1,137 @@
+/***************************************************************************
+ *            berkeley_db_benchmark.cpp
+ *
+ *  Mon Jul 10 16:01:47 2006
+ *  Copyright  2006  User Roman Dementiev
+ *  Email
+ ****************************************************************************/
+
+#include <stxxl>
+
+#define TOTAL_PQ_MEM_SIZE    (768*1024*1024)
+
+#define PQ_MEM_SIZE					(512*1024*1024)
+
+#define PREFETCH_POOL_SIZE 			((TOTAL_PQ_MEM_SIZE-PQ_MEM_SIZE)/2)
+#define WRITE_POOL_SIZE						(PREFETCH_POOL_SIZE)
+
+
+#define MAX_ELEMENTS (2000*1024*1024)
+
+
+struct my_record
+{
+	int key;
+	int data;
+	my_record() {}
+	my_record(int k, int d): key(k),data(d) { }
+};
+
+std::ostream & operator << (std::ostream & o, const my_record & obj)
+{
+	o << obj.key << " "<<obj.data;
+	return o;
+}
+
+bool operator == (const my_record & a, const my_record & b)
+{
+	return a.key == b.key;
+}
+
+bool operator != (const my_record & a, const my_record & b)
+{
+	return a.key != b.key;
+}
+
+bool operator < (const my_record & a, const my_record & b)
+{
+	return a.key < b.key;
+}
+
+bool operator > (const my_record & a, const my_record & b)
+{
+	return a.key>b.key;
+}
+
+
+
+struct comp_type
+{
+	bool operator () (const my_record & a, const my_record & b) const
+	{
+		return a>b;
+	}
+	static my_record min_value()
+	{ 
+		return my_record((std::numeric_limits<char>::max)(),0);
+	}
+};
+
+
+typedef stxxl::PRIORITY_QUEUE_GENERATOR<my_record,comp_type,
+	PQ_MEM_SIZE,MAX_ELEMENTS/1024>::result pq_type;
+
+typedef pq_type::block_type block_type;
+
+#define    BLOCK_SIZE block_type::raw_size
+
+
+#if 1
+unsigned ran32State = 0xdeadbeef;
+inline int myrand()
+{
+		return (ran32State = 1664525 * ran32State + 1013904223)&0x7fffffffUL;
+}
+#else // a longer pseudo random sequence
+long long unsigned ran32State = 0xdeadbeef;
+inline long long unsigned myrand()
+{
+		return (ran32State = (ran32State*0x5DEECE66DULL + 0xBULL)&0xFFFFFFFFFFFFULL );
+}
+#endif
+
+
+void run_stxxl_insert_all_delete_all(stxxl::int64 ops)
+{
+}
+
+
+int main(int argc, char * argv[])
+{
+    STXXL_MSG("stxxl::pq lock size: "<<BLOCK_SIZE<<" bytes")
+    
+   	#ifdef STXXL_DIRECT_IO_OFF
+	STXXL_MSG("STXXL_DIRECT_IO_OFF is defined")
+	#else
+	STXXL_MSG("STXXL_DIRECT_IO_OFF is NOT defined")
+	#endif
+	
+	if(argc < 3)
+	{
+		STXXL_MSG("Usage: "<<argv[0]<<" version #ops")
+		STXXL_MSG("\t version = 1: insert-all-delete-all stxxl map")
+		return 0;
+	}
+
+	int version = atoi(argv[1]);
+	stxxl::int64 ops = atoll(argv[2]);
+	
+	if(ops > MAX_ELEMENTS)
+	{
+		STXXL_MSG("#ops can not be larger than "<<MAX_ELEMENTS)
+		return 0;
+	}
+
+	STXXL_MSG("Running version      : "<<version)
+	STXXL_MSG("Operations to perform: "<<ops);
+	
+	switch(version)
+	{
+		case 1:
+			run_stxxl_insert_all_delete_all(ops);
+			break;
+		default:
+			STXXL_MSG("Unsupported version "<<version)
+	}
+	
+}
