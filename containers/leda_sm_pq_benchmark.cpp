0fa9e8e8c186f7eb60e7194844bb65cdb4b66000
Roman Dementiev <dementiev@ira.uka.de>
2006-07-13 13:10:51 +0000

    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@154 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ containers/leda_sm_pq_benchmark.cpp
@@ -0,0 +1,259 @@
+/***************************************************************************
+ *            
+ *
+ *  Mon Jul 10 16:01:47 2006
+ *  Copyright  2006  User Roman Dementiev
+ *  Email
+ ****************************************************************************/
+
+#include <LEDA-SM/ext_memory_manager.h>
+#include <LEDA-SM/ext_memory_manager.h>
+#include <LEDA-SM/block.h>
+#include <LEDA-SM/ext_array.h>
+#include <LEDA-SM/debug.h>
+#include <LEDA-SM/array_heap.h>
+#include <LEDA-SM/ext_r_heap.h>
+#include <LEDA-SM/buffer_tree.h>
+#include <LEDA/random.h>
+#include <algorithm>
+
+#include "../common/utils_ledasm.h"
+#include "../common/timer.h"
+
+
+#define PQ_MEM_SIZE	(512*1024*1024)
+
+
+#define MAX_ELEMENTS (2000*1024*1024)
+
+
+struct my_record
+{
+	int key;
+	int data;
+	my_record() {}
+	my_record(int k, int d): key(k),data(d) { }
+};
+
+std::ostream & operator << (std::ostream & o, const my_record & obj)
+{
+	o << obj.key << " "<<obj.data;
+	return o;
+}
+
+bool operator == (const my_record & a, const my_record & b)
+{
+	return a.key == b.key;
+}
+
+bool operator != (const my_record & a, const my_record & b)
+{
+	return a.key != b.key;
+}
+
+bool operator < (const my_record & a, const my_record & b)
+{
+	return a.key < b.key;
+}
+
+bool operator > (const my_record & a, const my_record & b)
+{
+	return a.key>b.key;
+}
+
+int compare(const my_record & a, const my_record & b)
+{
+  if(a.key != b.key)
+    return (a.key - b.key);
+  return (a.key - b.key);
+}
+
+
+#define    BLOCK_SIZE1 (EXT_BLK_SZ*4)
+#define    BLOCK_SIZE2 (DISK_BLOCK_SIZE*4)
+
+
+#if 1
+unsigned ran32State = 0xdeadbeef;
+inline int myrand()
+{
+		return ((int)((ran32State = 1664525 * ran32State + 1013904223)>>1))-1;
+}
+#else // a longer pseudo random sequence
+long long unsigned ran32State = 0xdeadbeef;
+inline long long unsigned myrand()
+{
+		return (ran32State = (ran32State*0x5DEECE66DULL + 0xBULL)&0xFFFFFFFFFFFFULL );
+}
+#endif
+
+int pq_blocks;
+
+void run_ledasm_insert_all_delete_all(stxxl::int64 ops)
+{
+	
+	array_heap<int,int> PQ(pq_blocks);
+	
+	stxxl::int64 i;
+	
+	my_record cur;
+	
+	
+	stxxl::timer Timer;
+	Timer.start();
+	
+	for(i=0;i<ops;++i)
+	{
+		PQ.insert(myrand(),0);
+	}
+	
+	Timer.stop();
+	
+	STXXL_MSG("Records in PQ: "<<PQ.size())
+	if(i != PQ.size())
+	{
+		STXXL_MSG("Size does not match")
+		abort();
+	}
+	
+	STXXL_MSG("Insertions elapsed time: "<<(Timer.mseconds()/1000.)<<
+				" seconds : "<< (double(ops)/(Timer.mseconds()/1000.))<<" key/data pairs per sec")
+	
+	ext_mem_mgr.print_statistics();
+	ext_mem_mgr.reset_statistics();
+	////////////////////////////////////////////////
+	Timer.reset();
+	
+	for(i=0;i<ops;++i)
+	{
+		PQ.del_min();
+	}
+	
+	Timer.stop();
+	
+	STXXL_MSG("Records in PQ: "<<PQ.size())
+	if(!PQ.empty())
+	{
+		STXXL_MSG("PQ must be empty")
+		abort();
+	}
+	
+	STXXL_MSG("Deletions elapsed time: "<<(Timer.mseconds()/1000.)<<
+				" seconds : "<< (double(ops)/(Timer.mseconds()/1000.))<<" key/data pairs per sec")
+	
+	
+	ext_mem_mgr.print_statistics();
+}
+
+
+void run_ledasm_intermixed(stxxl::int64 ops)
+{
+	
+	array_heap<int,int> PQ(pq_blocks);
+	
+	stxxl::int64 i;
+	
+	my_record cur;
+	
+	
+	
+	stxxl::timer Timer;
+	Timer.start();
+	
+	for(i=0;i<ops;++i)
+	{
+		PQ.insert(myrand(),0);
+	}
+	
+	Timer.stop();
+	
+	STXXL_MSG("Records in PQ: "<<PQ.size())
+	if(i != PQ.size())
+	{
+		STXXL_MSG("Size does not match")
+		abort();
+	}
+	
+	STXXL_MSG("Insertions elapsed time: "<<(Timer.mseconds()/1000.)<<
+				" seconds : "<< (double(ops)/(Timer.mseconds()/1000.))<<" key/data pairs per sec")
+	
+	ext_mem_mgr.print_statistics();
+	ext_mem_mgr.reset_statistics();
+	////////////////////////////////////////////////
+	Timer.reset();
+	
+	for(i=0;i<ops;++i)
+	{
+		int o = myrand()%3;
+		if(o == 0)
+		{
+			PQ.insert(myrand(),0);
+		}
+		else
+		{
+			assert(!PQ.empty());
+			PQ.del_min();
+		}
+	}
+	
+	Timer.stop();
+	
+	STXXL_MSG("Records in PQ: "<<PQ.size())
+	
+	STXXL_MSG("Deletions/Insertion elapsed time: "<<(Timer.mseconds()/1000.)<<
+				" seconds : "<< (double(ops)/(Timer.mseconds()/1000.))<<" key/data pairs per sec")
+	
+	
+	ext_mem_mgr.print_statistics();
+}
+
+int main(int argc, char * argv[])
+{
+    	STXXL_MSG("block size 1: "<<BLOCK_SIZE1<<" bytes")
+	STXXL_MSG("block size 2: "<<BLOCK_SIZE2<<" bytes")
+    
+   
+	if(argc < 3)
+	{
+		STXXL_MSG("Usage: "<<argv[0]<<" version #ops")
+		STXXL_MSG("\t version = 1: insert-all-delete-all leda-sm pqueue")
+		STXXL_MSG("\t version = 2: insert-all-delete-all leda-sm pqueue")
+		return 0;
+	}
+
+	int version = atoi(argv[1]);
+	stxxl::int64 ops = atoll(argv[2]);
+	
+	if(ops > MAX_ELEMENTS)
+	{
+		STXXL_MSG("#ops can not be larger than "<<MAX_ELEMENTS)
+		return 0;
+	}
+
+
+	pq_blocks = PQ_MEM_SIZE/(EXT_BLK_SZ*sizeof(GenPtr));
+
+        if(pq_blocks <= 500)
+        {
+          std::cout << "Array heap must have > 500 blocks, current number of blocks " <<
+              pq_blocks  << std::endl;
+          return -1;
+        }
+
+
+	STXXL_MSG("Running version      : "<<version)
+	STXXL_MSG("Operations to perform: "<<ops);
+	
+	switch(version)
+	{
+		case 1:
+			run_ledasm_insert_all_delete_all(ops);
+			break;
+		case 2:
+			run_ledasm_intermixed(ops);
+			break;
+		default:
+			STXXL_MSG("Unsupported version "<<version)
+	}
+	
+}
--- containers/stack_benchmark.cpp
+++ containers/stack_benchmark.cpp
@@ -31,6 +31,8 @@ void run_stxxl_growshrink2_stack(stxxl::int64 volume)
 	typedef typename stxxl::STACK_GENERATOR<my_record,stxxl::external,
 	stxxl::grow_shrink2,DISKS,BLOCK_SIZE>::result stack_type;
 	typedef typename stack_type::block_type block_type;
+
+	STXXL_MSG("Record size: "<<sizeof(my_record)<<" bytes")
 	
 	stxxl::prefetch_pool<block_type> p_pool(DISKS*8);
   	stxxl::write_pool<block_type>    w_pool(DISKS*8);
@@ -64,7 +66,8 @@ void run_stxxl_growshrink2_stack(stxxl::int64 volume)
 	}
 	
 	STXXL_MSG("Insertions elapsed time: "<<(Timer.mseconds()/1000.)<<
-				" seconds : "<< (double(ops)/(Timer.mseconds()/1000.))<<" key/data pairs per sec")
+				" seconds : "<< (double(volume)/(1024.*1024.*Timer.mseconds()/1000.))<<
+				" MB/s")
 	
 	std::cout << *Stats;
 	Stats->reset();
@@ -90,7 +93,80 @@ void run_stxxl_growshrink2_stack(stxxl::int64 volume)
 	}
 	
 	STXXL_MSG("Deletions elapsed time: "<<(Timer.mseconds()/1000.)<<
-				" seconds : "<< (double(ops)/(Timer.mseconds()/1000.))<<" key/data pairs per sec")
+				" seconds : "<< (double(volume)/(1024.*1024.*Timer.mseconds()/1000.))<<
+				" MB/s")
+	
+	std::cout << *Stats;
+	
+}
+
+
+template <class my_record>
+void run_stxxl_normal_stack(stxxl::int64 volume)
+{
+	typedef typename stxxl::STACK_GENERATOR<my_record,stxxl::external,
+	stxxl::normal,DISKS,BLOCK_SIZE>::result stack_type;
+	typedef typename stack_type::block_type block_type;
+
+	STXXL_MSG("Record size: "<<sizeof(my_record)<<" bytes")
+	
+	stack_type Stack;
+	
+	stxxl::int64 ops = volume/sizeof(my_record);
+	
+	stxxl::int64 i;
+	
+	my_record cur;
+	
+	stxxl::stats * Stats = stxxl::stats::get_instance();
+	Stats->reset();
+	
+	stxxl::timer Timer;
+	Timer.start();
+	
+	for(i=0;i<ops;++i)
+	{
+		Stack.push(cur);
+	}
+	
+	Timer.stop();
+	
+	STXXL_MSG("Records in Stack: "<<Stack.size())
+	if(i != Stack.size())
+	{
+		STXXL_MSG("Size does not match")
+		abort();
+	}
+	
+	STXXL_MSG("Insertions elapsed time: "<<(Timer.mseconds()/1000.)<<
+				" seconds : "<< (double(volume)/(1024.*1024.*Timer.mseconds()/1000.))<<
+				" MB/s")
+	
+	std::cout << *Stats;
+	Stats->reset();
+	
+	
+	////////////////////////////////////////////////
+	Timer.reset();
+	Timer.start();
+	
+	for(i=0;i<ops;++i)
+	{
+		Stack.pop();
+	}
+	
+	Timer.stop();
+	
+	STXXL_MSG("Records in Stack: "<<Stack.size())
+	if(!Stack.empty())
+	{
+		STXXL_MSG("Stack must be empty")
+		abort();
+	}
+	
+	STXXL_MSG("Deletions elapsed time: "<<(Timer.mseconds()/1000.)<<
+				" seconds : "<< (double(volume)/(1024.*1024.*Timer.mseconds()/1000.))<<
+				" MB/s")
 	
 	std::cout << *Stats;
 	
@@ -98,10 +174,9 @@ void run_stxxl_growshrink2_stack(stxxl::int64 volume)
 
 
 
-
 int main(int argc, char * argv[])
 {
-    STXXL_MSG("stxxl::pq lock size: "<<BLOCK_SIZE<<" bytes")
+    STXXL_MSG("stxxl::pq block size: "<<BLOCK_SIZE<<" bytes")
     
    	#ifdef STXXL_DIRECT_IO_OFF
 	STXXL_MSG("STXXL_DIRECT_IO_OFF is defined")
@@ -112,8 +187,10 @@ int main(int argc, char * argv[])
 	if(argc < 3)
 	{
 		STXXL_MSG("Usage: "<<argv[0]<<" version #volume")
-		STXXL_MSG("\t version = 1: grow-shrink-stack2 with 8 byte records")
-		STXXL_MSG("\t version = 2: grow-shrink-stack2 with 128 byte records")
+		STXXL_MSG("\t version = 1: grow-shrink-stack2 with 4 byte records")
+		STXXL_MSG("\t version = 2: grow-shrink-stack2 with 16 byte records")
+		STXXL_MSG("\t version = 3: normal-stack with 4 byte records")
+		STXXL_MSG("\t version = 4: normal-stack with 16 byte records")
 		return 0;
 	}
 
@@ -123,7 +200,7 @@ int main(int argc, char * argv[])
 	STXXL_MSG("Allocating array with size "<<MEM_2_RESERVE
 		<<" bytes to prevent file buffering.")
 	int * array = new int[MEM_2_RESERVE/sizeof(int)];
-	std::fill(array,array+MEM_2_RESERVE,0);
+	std::fill(array,array+(MEM_2_RESERVE/sizeof(int)),0);
 	
 	STXXL_MSG("Running version: "<<version)
 	STXXL_MSG("Data volume    : "<<volume <<" bytes");
@@ -131,10 +208,16 @@ int main(int argc, char * argv[])
 	switch(version)
 	{
 		case 1:
-			run_stxxl_growshrink2_stack<my_record_<8> >(volume);
+			run_stxxl_growshrink2_stack<my_record_<4> >(volume);
 			break;
 		case 2:
-			run_stxxl_growshrink2_stack<my_record_<128> >(volume);
+			run_stxxl_growshrink2_stack<my_record_<16> >(volume);
+			break;
+		case 3:
+			run_stxxl_normal_stack<my_record_<4> >(volume);
+			break;
+		case 4:
+			run_stxxl_normal_stack<my_record_<16> >(volume);
 			break;
 		default:
 			STXXL_MSG("Unsupported version "<<version)
