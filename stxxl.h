02e8e1093dea5227bad27b0013e7710259fcd5bf
Andreas Beckmann <beckmann@mpi-inf.mpg.de>
2007-06-12 09:24:11 +0000

    reorganize user interface headers, add some, use bits
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/branches/anbe/reorganize-includes@324 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ stxxl.h
@@ -0,0 +1,25 @@
+#ifndef STXXL_MAIN_HEADER
+#define STXXL_MAIN_HEADER
+
+#include "stxxl/bits/common/utils.h"
+
+#include "stxxl/io"
+
+#include "stxxl/mng"
+
+#include "stxxl/vector"
+#include "stxxl/stack"
+#include "stxxl/priority_queue"
+#include "stxxl/map"
+#include "stxxl/queue"
+#include "stxxl/deque"
+
+#include "stxxl/algorithm"
+
+#include "stxxl/stream"
+
+#include "stxxl/bits/common/debug.h"
+
+#include "stxxl/bits/common/timer.h"
+
+#endif // STXXL_MAIN_HEADER
