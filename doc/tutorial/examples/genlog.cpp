4e1b5bab7b9ae5607d5f1f97208bb6737cd5b903
Roman Dementiev <dementiev@ira.uka.de>
2007-07-12 11:59:33 +0000

    added source code of the examples from the tutorial
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@368 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ doc/tutorial/examples/genlog.cpp
@@ -0,0 +1,116 @@
+/***************************************************************************
+ *            genlog.cpp
+ *
+ *  Mon Jun 21 14:54:54 2004
+ *  Copyright  2004  Roman Dementiev
+ *  dementiev@mpi-sb.mpg.de
+ ****************************************************************************/
+
+#include <stxxl>
+
+#include <fstream>
+#include <time.h>
+
+struct LogEntry
+{
+  long long unsigned from; // callers number
+  long long unsigned to;   // destination number
+  time_t timestamp;        // time of event
+  int event;               // event type 1 - call started
+                           //            2 - call ended
+};
+
+struct cmp
+{
+	bool operator () (const LogEntry & a, const LogEntry & b) const
+	{
+		return a.timestamp < b.timestamp ;
+	}
+	static LogEntry min_value()
+	{
+		LogEntry e; e.timestamp = std::numeric_limits<time_t>::min(); return e;
+	}
+	static LogEntry max_value()
+	{
+		LogEntry e; e.timestamp = std::numeric_limits<time_t>::max(); return e;
+	}
+};
+
+typedef stxxl::VECTOR_GENERATOR<LogEntry,1,1>::result vector_type;
+
+std::ostream & operator << (std::ostream & i, const LogEntry & entry)
+{
+  i << entry.from << " ";
+  i << entry.to << " ";
+  i << entry.timestamp<< " ";
+  i << entry.event << "\n";
+  return i;
+}
+
+
+int main(int argc, char * argv[])
+{
+  if(argc < 5)
+  {
+	  STXXL_MSG("Usage: "<<argv[0]<<" ncalls avcalls main logfile")
+	  STXXL_MSG(" ncalls  - number of calls")
+	  STXXL_MSG(" avcalls - average number of calls per client")
+	  STXXL_MSG(" main    - memory to use (in MB)")
+	  STXXL_MSG(" logfile - file name of the output")
+	  
+	  return 0;
+  }
+  unsigned M = atol(argv[3])*1024*1024;
+  const long long int ncalls = atoll(argv[1]);
+  const int av_calls = atol(argv[2]);
+  const long long int nclients = ncalls/av_calls;
+  long long int calls_made = 0;
+  
+  time_t now = time(NULL);
+
+  vector_type log;
+  log.reserve(2*ncalls);
+  
+  stxxl::random_number<> rnd;
+
+  for(	long long int number = 0; 
+	  	number < nclients && calls_made < ncalls; 
+  		++number )
+  {
+    unsigned serv = std::min((long long int)rnd(av_calls*2),(ncalls - calls_made));
+    LogEntry e;
+    e.from = number;
+
+    time_t cur = now;
+
+    while(serv-- > 0)
+    {
+      	cur += 1 + rnd(3600*24);
+
+      	e.to = rnd(nclients);
+      	e.timestamp = cur;
+
+		++calls_made;
+		e.event = 1;
+		log.push_back(e);
+	
+		cur += 1 + rnd(1800);
+		e.timestamp = cur;
+		e.event = 2;
+	
+		log.push_back(e);
+    }
+  }
+  
+  // sort events in order of their appereance
+  stxxl::sort(log.begin(),log.end(),cmp(),M); 
+  
+  std::fstream out(argv[4],std::ios::out);
+
+  std::copy(log.begin(),log.end(),std::ostream_iterator<LogEntry>(out));
+  
+  STXXL_MSG("\n"<<calls_made<<" calls made.")
+  STXXL_MSG("The log is written to '"<<argv[4]<<"'.")
+  
+  return 0;
+}
---
+++ doc/tutorial/examples/phonebills.cpp
@@ -0,0 +1,157 @@
+/***************************************************************************
+ *            phonebills.cpp
+ *
+ *  Mon Jun 21 14:54:54 2004
+ *  Copyright  2004  Roman Dementiev
+ *  dementiev@mpi-sb.mpg.de
+ ****************************************************************************/
+
+#include <stxxl>         // Stxxl header
+#include <algorithm>     // for STL sort
+#include <vector>        // for STL vector
+
+#include <fstream>
+#include <time.h>
+
+#define CT_PER_MIN 2
+
+struct LogEntry
+{
+  long long int from; // callers number
+  long long int to;   // destination number
+  time_t timestamp;   // time of event
+  int event;          // event type 1 - call started
+                      //            2 - call ended
+};
+
+std::istream & operator >> (std::istream & i, LogEntry & entry)
+{
+  i >> entry.from;
+  i >> entry.to;
+  i >> entry.timestamp;
+  i >> entry.event;
+  return i;
+}
+
+std::ostream & operator << (std::ostream & i, const LogEntry & entry)
+{
+  i << entry.from << " ";
+  i << entry.to << " ";
+  i << entry.timestamp<< " ";
+  i << entry.event ;
+  return i;
+}
+
+struct ProduceBill
+{
+	std::ostream & out;
+	unsigned sum;
+	LogEntry last;
+	
+	ProduceBill(std::ostream & o_):out(o_),sum(0){ last.from = -1; }
+	
+	void operator () (const LogEntry & e)
+	{
+		if(last.from == e.from)
+		{
+			// either the last event was 'call started' and current event 
+			// is 'call ended' or the last event was 'call ended' and 
+			// current event is 'call started'
+			assert(	(last.event == 1 && e.event == 2) || 
+					(last.event == 2 && e.event == 1));	
+			
+			if(e.event == 2) // call ended
+				sum += CT_PER_MIN*(e.timestamp - last.timestamp)/60;
+		}
+		else if(last.from != -1)
+		{
+			assert(last.event == 2);// must be 'call ended'
+			assert(e.event == 1);   // must be 'call started'
+			
+			// output the total sum
+			out << last.from <<"; "<< (sum/100)<<" EUR "<< (sum%100)<< 
+				" ct"<< std::endl;
+			
+			sum = 0; // reset the sum
+		}
+			
+		last = e;
+	}
+};
+
+
+struct SortByCaller
+{
+  // comparison function
+  bool operator() (const LogEntry & a, const LogEntry & b) const
+  {
+	return  a.from < b.from || 
+	  		(a.from == b.from && a.timestamp < b.timestamp) ||
+	  		(a.from == b.from && a.timestamp == b.timestamp && 
+	  				a.event < b.event);
+  }
+  // min sentinel = value which is strictly smaller that any input element
+  static LogEntry min_value()
+  {
+	LogEntry e; e.from = std::numeric_limits<long long int>::min(); return e;
+  }
+  // max sentinel = value which is strictly larger that any input element
+  static LogEntry max_value()
+  {
+	LogEntry e; e.from = std::numeric_limits<long long int>::max(); return e;
+  }
+};
+
+
+#ifdef USE_STXXL
+typedef stxxl::vector<LogEntry> vector_type;
+#else
+typedef std::vector<LogEntry> vector_type;
+#endif
+
+
+void print_usage(const char * program)
+{
+	  std::cout << "Usage: "<<program<<" logfile main billfile" << std::endl;
+	  std::cout <<" logfile  - file name of the input"<< std::endl;
+	  std::cout <<" main     - memory to use (in MB)"<< std::endl;
+	  std::cout <<" billfile - file name of the output"<< std::endl;
+}
+
+
+int main(int argc, char * argv[])
+{
+  if(argc < 4)
+  {
+	  print_usage(argv[0]);
+	  return 0;
+  }
+  
+  // read from the file
+  std::fstream in(argv[1],std::ios::in);
+  vector_type v;
+  
+  std::copy(std::istream_iterator<LogEntry>(in),
+  			std::istream_iterator<LogEntry>(),
+  			std::back_inserter(v));
+  
+  // sort by callers
+  #ifndef USE_STXXL
+  
+  std::sort(v.begin(),v.end(),SortByCaller());
+  std::fstream out(argv[3],std::ios::out);
+  // output bills
+  std::for_each(v.begin(),v.end(),ProduceBill(out));
+  
+  #else
+  const unsigned M = atol(argv[2])*1024*1024;
+
+  stxxl::sort(v.begin(),v.end(),SortByCaller(),M);
+  std::fstream out(argv[3],std::ios::out);
+  // output bills
+  stxxl::for_each(v.begin(),v.end(),ProduceBill(out),M/vector_type::block_type::raw_size);
+  
+  #endif
+  
+  return 0;
+}
