d3591755a19055e4fb7a1eeb1c1ec787c8b29138
Andreas Beckmann <beckmann@mpi-inf.mpg.de>
2007-11-12 16:57:58 +0000

    use the random number generators
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@557 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ common/test_random.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stxxl/random>
+
+int main()
+{
+    srand48(time(NULL));
+    stxxl::random_number32 random_number32;
+    stxxl::random_uniform_fast random_uniform_fast;
+    stxxl::random_uniform_slow random_uniform_slow;
+    stxxl::random_number<> random_number_fast;
+    stxxl::random_number<stxxl::random_uniform_slow> random_number_slow;
+    stxxl::random_number64 random_number64;
+
+    for (int i = 0; i < 3; ++i) {
+        std::cout << "d48 " << drand48() << std::endl;
+        std::cout << "r32 " << random_number32() << std::endl;
+        std::cout << "ruf " << random_uniform_fast() << std::endl;
+        std::cout << "rus " << random_uniform_slow() << std::endl;
+        std::cout << "rnf " << random_number_fast(42) << std::endl;
+        std::cout << "rns " << random_number_slow(42) << std::endl;
+        std::cout << "r64 " << random_number64() << std::endl;
+        std::cout << std::endl;
+    }
+}
