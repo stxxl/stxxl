8a8c6c4749d7a4c54823166ae068c87ad1b6537a
Andreas Beckmann <beckmann@mpi-inf.mpg.de>
2008-02-22 14:56:10 +0000

    try another approach for intel_compatibility.h
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@626 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/common/intel_compatibility.h
+++ include/stxxl/bits/common/intel_compatibility.h
@@ -1,8 +1,8 @@
 #ifndef STXXL_INTEL_COMPATIBILITY_HEADER_INCLUDED
 #define STXXL_INTEL_COMPATIBILITY_HEADER_INCLUDED
 /***************************************************************************
- *   Copyright (C) 2007 by Johannes Singler                                *
- *   singler@ira.uka.de                                                    *
+ *   Copyright (C) 2007 by Johannes Singler  <singler@ira.uka.de>          *
+ *   Copyright (C) 2008 by Andreas Beckmann  <beckmann@mpi-inf.mpg.de>     *
  *   Distributed under the Boost Software License, Version 1.0.            *
  *   (See accompanying file LICENSE_1_0.txt or copy at                     *
  *   http://www.boost.org/LICENSE_1_0.txt)                                 *
@@ -12,13 +12,23 @@
 /** @file intel_compatibility.h
  *  @brief Intel compiler compatibility work-around. */
 
-#if defined(__ICC) && ! ( \
-        (defined(__x86_64) && (__ICC > 1010 || (__ICC == 1010 && __INTEL_COMPILER_BUILD_DATE >= 20080112))) \
-	)
-/** @brief Replacement of unknown atomic function. Bug fixed for x86_64 in icpc 10.1.012. */
-#ifndef __sync_fetch_and_add
+#if defined(__ICC)
+
+/** @brief Fake function that will cause linking failure if used. */
+template <typename T1, typename T2>
+T1 __in_icc_there_is_no__sync_fetch_and_add(T1 *, T2);
+
+#if 0
+// replacing the overloaded builtin function 'T __sync_fetch_and_add(T*, T)'
+// with 'int32 _InterlockedExchangeAdd(int32, int32)' is not a good idea
 #define __sync_fetch_and_add(ptr,addend) _InterlockedExchangeAdd(const_cast<void*>(reinterpret_cast<volatile void*>(ptr)), addend);
 #endif
+
+/** @brief Replacement of unknown atomic builtin function.
+ *         Should work as long as it isn't instantiated. */
+#undef __sync_fetch_and_add
+#define __sync_fetch_and_add __in_icc_there_is_no__sync_fetch_and_add
+
 #endif
 
 #endif // !STXXL_INTEL_COMPATIBILITY_HEADER_INCLUDED
