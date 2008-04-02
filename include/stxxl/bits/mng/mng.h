2fef8e60ad4e1fa1a60c409aa2e1c31a7f5aa320
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-04-02 08:12:23 +0000

    made BIDArray, DiskAllocator and singletons noncopyable
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@671 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/mng/mng.h
+++ include/stxxl/bits/mng/mng.h
@@ -386,7 +386,7 @@ public:
  */
 
 template<unsigned BLK_SIZE>
-class BIDArray
+class BIDArray : private noncopyable
 {
 protected:
     unsigned_type _size;
@@ -446,7 +446,7 @@ public:
 };
 
 
-class DiskAllocator
+class DiskAllocator : private noncopyable
 {
 #ifdef STXXL_BOOST_THREADS
     boost::mutex mutex;
@@ -824,7 +824,7 @@ void DiskAllocator::delete_block (const BID < BLK_SIZE > &bid)
 
 //! \brief Access point to disks properties
 //! \remarks is a singleton
-class config
+class config : private noncopyable
 {
     struct DiskEntry
     {
@@ -1152,7 +1152,7 @@ struct offset_allocator
 
 //! Manages allocation and deallocation of blocks in multiple/single disk setting
 //! \remarks is a singleton
-class block_manager
+class block_manager : private noncopyable
 {
     DiskAllocator * *disk_allocators;
     file * * disk_files;
