2da498df210e2bd0645e72cb1923f1d6d349afb9
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-04-08 14:48:38 +0000

    a small helper utility to dump LOG2<i> results
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@694 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ utils/log2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stxxl.h>
+
+using stxxl::LOG2;
+using stxxl::unsigned_type;
+
+template <unsigned_type i>
+void log_i()
+{
+    std::cout << i << "\t" << (i < 1000000 ? "\t" : "")
+        << stxxl::LOG2_floor<i>::value << "\t"
+        << stxxl::LOG2<i>::floor << "\t"
+        << stxxl::LOG2<i>::ceil << std::endl;
+}
+
+template <unsigned_type i>
+void log_ipm1()
+{
+    log_i<i-1>();
+    log_i<i>();
+    log_i<i+1>();
+    std::cout << std::endl;
+}
+
+int main()
+{
+    std::cout << "i\t\tLOG2<i>\tLOG2<i>\tLOG2<i>" << std::endl;
+    std::cout << "\t\t\tfloor\tceil" << std::endl;
+    log_ipm1<1 << 0>();
+    log_ipm1<1 << 1>();
+    log_ipm1<1 << 2>();
+    log_ipm1<1 << 3>();
+    log_ipm1<1 << 4>();
+    log_ipm1<1 << 5>();
+    log_ipm1<1 << 6>();
+    log_ipm1<1 << 7>();
+    log_ipm1<1 << 8>();
+    log_ipm1<1 << 9>();
+    log_ipm1<1 << 10>();
+    log_ipm1<1 << 11>();
+    log_ipm1<1 << 12>();
+    log_ipm1<1 << 16>();
+    log_ipm1<1 << 24>();
+    log_ipm1<1 << 30>();
+    log_ipm1<1 << 31>();
+#if __WORDSIZE == 64
+    log_ipm1<1L << 32>();
+    log_ipm1<1L << 33>();
+    log_ipm1<1L << 48>();
+    log_ipm1<1L << 63>();
+#endif
+}
+
