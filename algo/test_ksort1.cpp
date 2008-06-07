ea24fb549711411372c913ab0fffd09b9df3eec5
Roman Dementiev <roman.dementiev@gmail.com>
2008-06-07 17:08:36 +0000

    minor changes in test_ksort1, use numeric_limits
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@841 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- algo/test_ksort1.cpp
+++ algo/test_ksort1.cpp
@@ -1,6 +1,7 @@
 #include "stxxl/mng"
 #include "stxxl/ksort"
 #include "stxxl/vector"
+#include "limits"
 
 //! \example algo/test_ksort1.cpp
 //! This is an example of how to use \c stxxl::ksort() algorithm
@@ -21,8 +22,8 @@ struct my_type
     my_type() : _key(0), _key_copy(0) { };
     my_type(key_type1 __key) : _key(__key) { };
 
-    //my_type min_value() const { return my_type(0); };
-    //my_type max_value() const { return my_type(0xffffffff); };
+    my_type min_value() const { return my_type((std::numeric_limits<key_type1>::min)()); };
+    my_type max_value() const { return my_type((std::numeric_limits<key_type1>::max)()); };
 };
 
 std::ostream & operator << (std::ostream & o, const my_type & obj)
@@ -34,17 +35,18 @@ std::ostream & operator << (std::ostream & o, const my_type & obj)
 struct get_key
 {
     typedef my_type::key_type1 key_type;
+    my_type dummy;
     key_type operator()  (const my_type & obj) const
     {
         return obj._key;
     }
     my_type min_value() const
     {
-        return my_type(0);
+        return my_type((dummy.min_value)());
     }
     my_type max_value() const
     {
-        return my_type(0xffffffff);
+        return my_type((dummy.max_value)());
     }
 };
 
@@ -56,21 +58,30 @@ bool operator < (const my_type & a, const my_type & b)
 
 int main()
 {
+	STXXL_MSG("Check config...");
+	try {
+	stxxl::block_manager::get_instance();
+	}
+	catch (std::exception & e)
+	{
+		STXXL_MSG("Exception: "<< e.what());
+		abort();
+	}
     unsigned memory_to_use = 32 * 1024 * 1024;
-    typedef stxxl::VECTOR_GENERATOR < my_type, 2, 2 > ::result vector_type;
+    typedef stxxl::VECTOR_GENERATOR < my_type, 4, 4 > ::result vector_type;
     const stxxl::int64 n_records = 3 * 32 * stxxl::int64(1024 * 1024) / sizeof(my_type);
     vector_type v(n_records);
 
-    stxxl::random_number64 rnd;
+    stxxl::random_number32 rnd;
     STXXL_MSG("Filling vector... ");
     for (vector_type::size_type i = 0; i < v.size(); i++)
     {
-        v[i]._key = rnd();
+        v[i]._key = rnd() + 1;
         v[i]._key_copy = v[i]._key;
     }
 
-    //STXXL_MSG("Checking order...");
-    //STXXL_MSG(((stxxl::is_sorted(v.begin(),v.end()))?"OK":"WRONG" ));
+    STXXL_MSG("Checking order...");
+    STXXL_MSG(((stxxl::is_sorted(v.begin(),v.end()))?"OK":"WRONG" ));
 
     STXXL_MSG("Sorting...");
     stxxl::ksort(v.begin(), v.end(), get_key(), memory_to_use);
@@ -90,7 +101,7 @@ int main()
         }
         if (i > 0 && prev._key == v[i]._key)
         {
-            STXXL_MSG("Duplicate at position " << i);
+            STXXL_MSG("Duplicate at position " << i <<" key=" << v[i]._key);
             //abort();
         }
         prev = v[i];
