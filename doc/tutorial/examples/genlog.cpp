40e6a5c9a1510a4b6c8c5b82033464fd5c72c132
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-06-19 16:24:43 +0000

    don't forget the copyright/license header in doc/tutorial/examples
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@946 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- doc/tutorial/examples/genlog.cpp
+++ doc/tutorial/examples/genlog.cpp
@@ -1,10 +1,14 @@
 /***************************************************************************
  *            genlog.cpp
  *
- *  Mon Jun 21 14:54:54 2004
- *  Copyright  2004  Roman Dementiev
- *  dementiev@mpi-sb.mpg.de
- ****************************************************************************/
+ *  Part of the STXXL. See http://stxxl.sourceforge.net
+ *
+ *  Copyright (C) 2004 Roman Dementiev <dementiev@mpi-sb.mpg.de>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************/
 
 #include <stxxl.h>
 
