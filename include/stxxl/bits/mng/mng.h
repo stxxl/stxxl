b167b7bd787126ab68e785f53fc85485a7cde45c
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-07-02 17:01:50 +0000

    rename a define
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1013 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/mng/mng.h
+++ include/stxxl/bits/mng/mng.h
@@ -329,7 +329,7 @@ public:
         STXXL_VERBOSE1("typed::block operator new[]: Meta info size: " << meta_info_size);
 
         void * result = aligned_alloc<BLOCK_ALIGN>(bytes, meta_info_size);
-        #ifdef STXXL_INITIALIZE_BLOCKS_FOR_VALGRIND
+        #ifdef STXXL_VALGRIND_TYPED_BLOCK_INITIALIZE_ZERO
         memset(result, 0, bytes);
         #endif
         char * tmp = (char *)result;
@@ -349,7 +349,7 @@ public:
         STXXL_VERBOSE1("typed::block operator new: Meta info size: " << meta_info_size);
 
         void * result = aligned_alloc<BLOCK_ALIGN>(bytes, meta_info_size);
-        #ifdef STXXL_INITIALIZE_BLOCKS_FOR_VALGRIND
+        #ifdef STXXL_VALGRIND_TYPED_BLOCK_INITIALIZE_ZERO
         memset(result, 0, bytes);
         #endif
         char * tmp = (char *)result;
