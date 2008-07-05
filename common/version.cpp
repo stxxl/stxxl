251eea9410099ab37291615a76c096974ce50ed6
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-07-05 13:40:30 +0000

    bump version to 1.2.1
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1027 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- common/version.cpp
+++ common/version.cpp
@@ -16,7 +16,7 @@
 #include <boost/version.hpp>
 #endif
 
-#define STXXL_VERSION_STRING_MA_MI_PL "1.2.0"
+#define STXXL_VERSION_STRING_MA_MI_PL "1.2.1"
 
 // version.defs gets created if a snapshot/beta/rc/release is done
 #ifdef HAVE_VERSION_DEFS
