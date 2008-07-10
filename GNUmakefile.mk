a9d5ce4109b87bfce48ad4650053950a41d97f6b
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-07-10 17:20:59 +0000

    initial g++-4.3 parallel mode support
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1046 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- GNUmakefile.mk
+++ GNUmakefile.mk
@@ -15,24 +15,38 @@ MODE	?= icpc
 
 NICE	?= nice
 
+PMODE	?= parallel_mode # undefine to disable
 MCSTL	?= mcstl # undefine to disable
 
 
 default-all: lib tests header-compile-test
 
 lib:
-	$(MAKE) -f Makefile library_$(MODE) $(if $(MCSTL),library_$(MODE)_mcstl)
+	$(MAKE) -f Makefile library_$(MODE) $(if $(PMODE),library_$(MODE)_pmode) $(if $(MCSTL),library_$(MODE)_mcstl)
 
 tests: lib
-	$(NICE) $(MAKE) -f Makefile tests_$(MODE) $(if $(MCSTL),tests_$(MODE)_mcstl)
+	$(NICE) $(MAKE) -f Makefile tests_$(MODE) $(if $(PMODE),tests_$(MODE)_pmode) $(if $(MCSTL),tests_$(MODE)_mcstl)
 
 header-compile-test: lib
 	$(NICE) $(MAKE) -C test/compile-stxxl-headers
+	$(if $(PMODE),$(NICE) $(MAKE) -C test/compile-stxxl-headers INSTANCE=pmstxxl)
 	$(if $(MCSTL),$(NICE) $(MAKE) -C test/compile-stxxl-headers INSTANCE=mcstxxl)
 
 clean:
-	$(MAKE) -f Makefile clean_$(MODE) clean_$(MODE)_mcstl clean_doxy
+	$(MAKE) -f Makefile clean_$(MODE) clean_$(MODE)_pmode clean_$(MODE)_mcstl clean_doxy
 	$(MAKE) -C test/compile-stxxl-headers clean
+
+GNUmakefile:
+	echo "" > $@
+	echo "#MODE=g++" >> $@
+	echo "#MODE=icpc" >> $@
+	echo "#NICE=" >> $@
+	echo "#PMODE=#empty" >> $@
+	echo "#MCSTL=#empty" >> $@
+	echo "" >> $@
+	echo "all: lib header-compile-test tests" >> $@
+	echo "-include make.settings.local" >> $@
+	echo "include GNUmakefile.mk" >> $@
 
 %::
 	$(MAKE) -f Makefile $@
