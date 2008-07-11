a1c4e8d95f9bea0e08003e0baf85f58805f96fbd
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-07-11 14:20:09 +0000

    add more generic singleton handling, including destroying on program exit
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1053 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ include/stxxl/bits/singleton.h
@@ -0,0 +1,72 @@
+/***************************************************************************
+ *  include/stxxl/bits/singleton.h
+ *
+ *  Part of the STXXL. See http://stxxl.sourceforge.net
+ *
+ *  Copyright (C) 2008 Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************/
+
+#ifndef STXXL_SINGLETON_HEADER
+#define STXXL_SINGLETON_HEADER
+
+#include <cstdlib>
+
+#include <stxxl/types>
+#include <stxxl/bits/noncopyable.h>
+
+
+__STXXL_BEGIN_NAMESPACE
+
+template <typename INSTANCE, bool destroy_on_exit = true>
+class singleton : private noncopyable
+{
+    typedef INSTANCE instance_type;
+    typedef instance_type * instance_pointer;
+
+    static instance_pointer instance;
+
+    static instance_pointer create_instance();
+    static void destroy_instance();
+
+public:
+    inline static instance_pointer get_instance()
+    {
+        if (!instance)
+            return create_instance();
+
+        return instance;
+    }
+};
+
+template <typename INSTANCE, bool destroy_on_exit>
+typename singleton<INSTANCE, destroy_on_exit>::instance_pointer
+singleton<INSTANCE, destroy_on_exit>::create_instance()
+{
+        if (!instance) {
+            instance = new instance_type();
+            if (destroy_on_exit)
+                atexit(destroy_instance);
+        }
+        return instance;
+}
+
+template <typename INSTANCE, bool destroy_on_exit>
+void singleton<INSTANCE, destroy_on_exit>::destroy_instance()
+{
+        instance_pointer inst = instance;
+	//instance = NULL;
+        instance = reinterpret_cast<instance_pointer>(unsigned_type(-1)); // bomb if used again
+        delete inst;
+}
+
+template <typename INSTANCE, bool destroy_on_exit>
+INSTANCE * singleton<INSTANCE, destroy_on_exit>::instance = NULL;
+
+__STXXL_END_NAMESPACE
+
+#endif // !STXXL_SINGLETON_HEADER
+
--- make.settings.gnu
+++ make.settings.gnu
@@ -261,7 +261,7 @@ CPPUNIT_LINKER_OPTIONS		+= -lcppunit -ldl
 
 HEADER_FILES_BITS	+= namespace.h noncopyable.h version.h
 HEADER_FILES_BITS	+= compat_hash_map.h compat_hash_set.h
-HEADER_FILES_BITS	+= compat_auto_ptr.h parallel.h
+HEADER_FILES_BITS	+= compat_auto_ptr.h parallel.h singleton.h
 
 HEADER_FILES_COMMON	+= aligned_alloc.h mutex.h rand.h semaphore.h state.h
 HEADER_FILES_COMMON	+= timer.h utils.h simple_vector.h
