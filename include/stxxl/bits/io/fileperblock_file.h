69b069e5ca2784a4094284d8c17c763d29b6b681
Johannes Singler <singler@ira.uka.de>
2008-10-16 07:46:00 +0000

    Cleanup, comments, cosmetics.
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/branches/file_per_block@1217 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/io/fileperblock_file.h
+++ include/stxxl/bits/io/fileperblock_file.h
@@ -26,7 +26,8 @@ __STXXL_BEGIN_NAMESPACE
 template<class base_file_type>
 class fileperblock_request;
 
-//! \brief Implementation of file based on other files, dynamically allocated one per block
+//! \brief Implementation of file based on other files, dynamically allocate one file per block.
+//! Allows for dynamic disk space consumption.
 template<class base_file_type>
 class fileperblock_file : public file
 {
@@ -37,14 +38,21 @@ private:
     int mode;
     unsigned_type block_size;
     int disk;
-    unsigned_type size_;
+    unsigned_type current_size;
 
 protected:
-    std::string file_name_for_block(unsigned_type);
+    //! \brief Constructs a file name for a given block.
+    std::string file_name_for_block(unsigned_type offset);
+
+    //! \brief Check whether the addresses are feasible in for this configuration.
+    bool feasible(int64 offset, size_t length);
+
+    //! \brief Computes the block-local offset.
+    int64 block_offset(int64 offset);
 
 public:
     //! \brief constructs file object
-    //! \param filename path of file(s), will append to it
+    //! \param filename base filename, numbering will be appended to it
     //! \param mode open mode, see \c file::open_modes
     //! \param disk disk(file) identifier
     fileperblock_file(
@@ -52,30 +60,45 @@ public:
         int mode,
         unsigned_type block_size,
         int disk = -1);
+
+    //! \brief Schedules asynchronous read request to the file
+    //! \param buffer pointer to memory buffer to read into
+    //! \param offset starting file position to read
+    //! \param length number of length to transfer
+    //! \param on_completion I/O completion handler
+    //! \return \c request_ptr object, that can be used to track the status of the operation
     virtual request_ptr aread(
         void * buffer,
-        int64 pos,
-        size_t bytes,
-        completion_handler on_cmpl);
+        int64 offset,
+        size_t length,
+        completion_handler on_completion);
+
+    //! \brief Schedules asynchronous write request to the file
+    //! \param buffer pointer to memory buffer to write from
+    //! \param offset starting file position to write
+    //! \param length number of length to transfer
+    //! \param on_completion I/O completion handler
+    //! \return \c request_ptr object, that can be used to track the status of the operation
     virtual request_ptr awrite(
         void * buffer,
-        int64 pos,
-        size_t bytes,
-        completion_handler on_cmpl);
+        int64 offset,
+        size_t length,
+        completion_handler on_completion);
+
     //! \brief Changes the size of the file
-    //! \param newsize value of the new file size
-    virtual void set_size(int64 newsize) { size_ = newsize; }
+    //! \param new_size value of the new file size
+    virtual void set_size(int64 new_size) { current_size = new_size; }
+
     //! \brief Returns size of the file
-    //! \return file size in bytes
-    virtual int64 size() { return size_;}
+    //! \return file size in length
+    virtual int64 size() { return current_size;}
 
-    virtual void delete_region(int64 offset, unsigned_type size);
-
-    void feasible(int64 pos, size_t bytes);
-    int64 block_offset(int64 pos);
+    //! \brief Frees the specified region.
+    //! Actually deletes the corresponding file if the whole thing is deleted.
+    virtual void delete_region(int64 offset, unsigned_type length);
 };
 
-//! \brief Implementation of request based on UNIX fileperblocks
+//! \brief Request type associated with fileperblock_file.
 template<class base_file_type>
 class fileperblock_request : public request
 {
@@ -87,22 +110,43 @@ class fileperblock_request : public request
     request_ptr base_request;
 
 protected:
+    //! Constructs the request.
+    //! \param f virtual file to access the data from.
+    //! \param buffer buffer to write/read the data to/from.
+    //! \param offset offset in the virtual file.
+    //! \param length number of bytes of the request.
+    //! \param read_or_write read or write the block?
+    //! \param on_completion handler to call on completion.
     fileperblock_request(
         fileperblock_file<base_file_type>* f,
-        void * buf,
-        int64 off,
-        size_t b,
-        typename base_request_type::request_type t,
-        completion_handler on_cmpl);
+        void * buffer,
+        int64 offset,
+        size_t length,
+        typename base_request_type::request_type read_or_write,
+        completion_handler on_completion);
+
     ~fileperblock_request();
+
+    //! \brief Serve the request, i. e. actually bring in the data.
+    //! Will block thread until the operation is finished.
     virtual void serve();
 
 public:
+    //! \brief Identifies the type of request I/O implementation
+    //! \return pointer to null terminated string of characters, containing the name of I/O implementation
     virtual const char * io_type();
 
+    //! \brief Add one waiter to this request.
     virtual bool add_waiter(onoff_switch * sw);
+
+    //! \brief Remove one waiter from this request.
     virtual void delete_waiter(onoff_switch * sw);
+
+    //! \brief Suspends calling thread until completion of the request
     virtual void wait();
+
+    //! \brief Polls the status of the request.
+    //! \return \c true if request is completed, otherwise \c false
     virtual bool poll();
 };
 
--- io/fileperblock_file.cpp
+++ io/fileperblock_file.cpp
@@ -31,47 +31,49 @@ template<class base_file_type>
 std::string fileperblock_file<base_file_type>::file_name_for_block(unsigned_type offset)
 {
     std::ostringstream name;
-    name << filename << "_fpb_" << std::setw(10) << std::setfill('0') << (offset / block_size);
+    //enough for 1 billion blocks
+    name << filename << "_fpb_" << std::setw(9) << std::setfill('0') << (offset / block_size);
     return name.str();
 }
 
 template<class base_file_type>
 request_ptr fileperblock_file<base_file_type>::aread(
     void * buffer,
-    stxxl::int64 pos,
-    size_t bytes,
-    completion_handler on_cmpl)
+    stxxl::int64 offset,
+    size_t length,
+    completion_handler on_completion)
 {
     request_ptr req = new fileperblock_request<base_file_type>(this,
-                                          buffer, pos, bytes,
-                                          request::READ, on_cmpl);
-
+                                          buffer, offset, length,
+                                          request::READ, on_completion);
     return req;
 }
 
 template<class base_file_type>
 request_ptr fileperblock_file<base_file_type>::awrite(
     void * buffer,
-    stxxl::int64 pos,
-    size_t bytes,
-    completion_handler on_cmpl)
+    stxxl::int64 offset,
+    size_t length,
+    completion_handler on_completion)
 {
-    request_ptr req = new fileperblock_request<base_file_type>(this, buffer, pos, bytes,
-                                          request::WRITE, on_cmpl);
+    request_ptr req = new fileperblock_request<base_file_type>(this,
+                                          buffer, offset, length,
+                                          request::WRITE, on_completion);
     return req;
 }
 
 template<class base_file_type>
-void fileperblock_file<base_file_type>::delete_region(int64 offset, unsigned_type size)
+void fileperblock_file<base_file_type>::delete_region(int64 offset, unsigned_type length)
 {
-    if(size == block_size)
+    if(length == block_size)
         ::remove(file_name_for_block(offset).c_str());
+    STXXL_VERBOSE0("delete_region " << offset << " + " << length)
 }
 
 template<class base_file_type>
-void fileperblock_file<base_file_type>::feasible(int64 offset, size_t size)
+bool fileperblock_file<base_file_type>::feasible(int64 offset, size_t length)
 {
-    assert((offset % block_size + size) <= block_size);
+    return (offset % block_size + length) <= block_size;
 }
 
 template<class base_file_type>
@@ -85,19 +87,19 @@ int64 fileperblock_file<base_file_type>::block_offset(int64 offset)
 template<class base_file_type>
 fileperblock_request<base_file_type>::fileperblock_request(
     fileperblock_file<base_file_type>* f,
-    void * buf,
-    stxxl::int64 off,
-    size_t b,
-    typename base_file_type::request::request_type t,
-    completion_handler on_cmpl)
+    void * buffer,
+    stxxl::int64 offset,
+    size_t length,
+    typename base_file_type::request::request_type read_or_write,
+    completion_handler on_completion)
    :
-    request(on_cmpl, f, buf, off, b, t),
-    base_file (new base_file_type(f->file_name_for_block(off), f->mode, f->disk)),
-    base_request(t == request::READ ?
-        base_file->aread(buf, f->block_offset(off), b, on_cmpl) :
-        base_file->awrite(buf, f->block_offset(off), b, on_cmpl))
+    request(on_completion, f, buffer, offset, length, read_or_write),
+    base_file (new base_file_type(f->file_name_for_block(offset), f->mode, f->disk)),
+    base_request(read_or_write == request::READ ?
+        base_file->aread(buffer, f->block_offset(offset), length, on_completion) :
+        base_file->awrite(buffer, f->block_offset(offset), length, on_completion))
 {
-    f->feasible(off, b);
+    assert(f->feasible(offset, length));
 }
 
 template<class base_file_type>
@@ -117,7 +119,6 @@ const char * fileperblock_request<base_file_type>::io_type()
 {
     return "fileperblock";
 }
-
 
 template<class base_file_type>
 bool fileperblock_request<base_file_type>::add_waiter(onoff_switch * sw)
