6882210b60d08fbd424874f91c7f64ab2746af7b
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-11-25 10:30:36 +0000

    separate the file::aread()/file::awrite() implementation from syscall_file
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1316 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ include/stxxl/bits/io/file_request_basic.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *  include/stxxl/bits/io/file_request_basic.h
+ *
+ *  Part of the STXXL. See http://stxxl.sourceforge.net
+ *
+ *  Copyright (C) 2008 Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************/
+
+#ifndef STXXL_HEADER_IO_FILE_REQUEST_BASIC
+#define STXXL_HEADER_IO_FILE_REQUEST_BASIC
+
+#include <stxxl/bits/io/file.h>
+
+
+__STXXL_BEGIN_NAMESPACE
+
+//! \addtogroup fileimpl
+//! \{
+
+//! \brief Implementation of some file methods based on request_impl_basic
+class file_request_basic : public file
+{
+public:
+    request_ptr aread(
+        void * buffer,
+        stxxl::int64 pos,
+        size_t bytes,
+        completion_handler on_cmpl);
+    request_ptr awrite(
+        void * buffer,
+        stxxl::int64 pos,
+        size_t bytes,
+        completion_handler on_cmpl);
+};
+
+//! \}
+
+__STXXL_END_NAMESPACE
+
+#endif // !STXXL_HEADER_IO_FILE_REQUEST_BASIC
+// vim: et:ts=4:sw=4
---
+++ io/file_request_basic.cpp
@@ -0,0 +1,51 @@
+/***************************************************************************
+ *  io/file_request_basic.cpp
+ *
+ *  Part of the STXXL. See http://stxxl.sourceforge.net
+ *
+ *  Copyright (C) 2008 Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************/
+
+#include <stxxl/bits/io/file_request_basic.h>
+#include <stxxl/bits/io/request_impl_basic.h>
+#include <stxxl/bits/io/disk_queues.h>
+
+
+__STXXL_BEGIN_NAMESPACE
+
+
+request_ptr file_request_basic::aread(
+    void * buffer,
+    stxxl::int64 pos,
+    size_t bytes,
+    completion_handler on_cmpl)
+{
+    request_ptr req = new request_impl_basic(on_cmpl, this,
+                                          buffer, pos, bytes,
+                                          request::READ);
+
+    disk_queues::get_instance()->add_request(req, get_id());
+
+    return req;
+}
+
+request_ptr file_request_basic::awrite(
+    void * buffer,
+    stxxl::int64 pos,
+    size_t bytes,
+    completion_handler on_cmpl)
+{
+    request_ptr req = new request_impl_basic(on_cmpl, this, buffer, pos, bytes,
+                                          request::WRITE);
+
+    disk_queues::get_instance()->add_request(req, get_id());
+
+    return req;
+}
+
+__STXXL_END_NAMESPACE
+// vim: et:ts=4:sw=4
