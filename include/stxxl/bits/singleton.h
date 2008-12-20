1e85d7a6628cad704eddc2e815dd1f7590d23be4
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2008-12-20 16:40:02 +0000

    add a mutex to singleton class to serialize updates of the instance pointer, fixes possible race condition when creating the instances
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1373 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/singleton.h
+++ include/stxxl/bits/singleton.h
@@ -17,6 +17,7 @@
 
 #include <stxxl/types>
 #include <stxxl/bits/noncopyable.h>
+#include <stxxl/bits/common/mutex.h>
 
 
 __STXXL_BEGIN_NAMESPACE
@@ -28,6 +29,7 @@ class singleton : private noncopyable
     typedef instance_type * instance_pointer;
 
     static instance_pointer instance;
+    static mutex instance_mutex; // prevent concurrent writes only
 
     static instance_pointer create_instance();
     static void destroy_instance();
@@ -46,6 +48,7 @@ template <typename INSTANCE, bool destroy_on_exit>
 typename singleton<INSTANCE, destroy_on_exit>::instance_pointer
 singleton<INSTANCE, destroy_on_exit>::create_instance()
 {
+    scoped_mutex_lock instance_write_lock(instance_mutex);
     if (!instance) {
         instance = new instance_type();
         if (destroy_on_exit)
@@ -57,6 +60,7 @@ singleton<INSTANCE, destroy_on_exit>::create_instance()
 template <typename INSTANCE, bool destroy_on_exit>
 void singleton<INSTANCE, destroy_on_exit>::destroy_instance()
 {
+    scoped_mutex_lock instance_write_lock(instance_mutex);
     instance_pointer inst = instance;
     //instance = NULL;
     instance = reinterpret_cast<instance_pointer>(unsigned_type(-1));     // bomb if used again
@@ -65,6 +69,9 @@ void singleton<INSTANCE, destroy_on_exit>::destroy_instance()
 
 template <typename INSTANCE, bool destroy_on_exit>
 INSTANCE * singleton<INSTANCE, destroy_on_exit>::instance = NULL;
+
+template <typename INSTANCE, bool destroy_on_exit>
+mutex singleton<INSTANCE, destroy_on_exit>::instance_mutex;
 
 __STXXL_END_NAMESPACE
 
