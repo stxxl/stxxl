a54f18fdae14e3e5af60411098fdae59b9b4e425
Johannes Singler <singler@ira.uka.de>
2009-01-28 09:33:37 +0000

    Refactoring.
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1390 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- io/benchmark_disks.cpp
+++ io/benchmark_disks.cpp
@@ -140,24 +140,24 @@ int main(int argc, char * argv[])
     const unsigned ndisks = disks_arr.size();
     //const stxxl::int64 max_mem = 8LL * 1024 * MB;
     const stxxl::int64 max_mem = 900 * MB;
-    //stxxl::int64 buffer_size = 1024 * MB;
-    stxxl::int64 buffer_size = 256 * MB;
-    while (buffer_size * ndisks > max_mem)
-        buffer_size >>= 1;
-    const stxxl::int64 buffer_size_int = buffer_size / sizeof(int);
+    //stxxl::int64 step_size = 1024 * MB;
+    stxxl::int64 step_size = 256 * MB;
+    while (step_size * ndisks > max_mem)
+        step_size >>= 1;
+    const stxxl::int64 step_size_int = step_size / sizeof(int);
 
-    const unsigned min_chunk = 8 * MB;
-    unsigned chunks = 32;
-    while (buffer_size / chunks < min_chunk)
-        chunks >>= 1;
-    const unsigned chunk_size = buffer_size / chunks;
-    const unsigned chunk_size_int = chunk_size / sizeof(int);
+    const unsigned min_block_size = 8 * MB;
+    unsigned num_blocks = 32;
+    while (step_size / num_blocks < min_block_size)
+        num_blocks >>= 1;
+    const unsigned block_size = step_size / num_blocks;
+    const unsigned block_size_int = block_size / sizeof(int);
 
     unsigned i, j;
 
-    unsigned * buffer = (unsigned *)stxxl::aligned_alloc<BLOCK_ALIGN>(buffer_size * ndisks);
+    unsigned * buffer = (unsigned *)stxxl::aligned_alloc<BLOCK_ALIGN>(step_size * ndisks);
     file ** disks = new file *[ndisks];
-    request_ptr * reqs = new request_ptr[ndisks * chunks];
+    request_ptr * reqs = new request_ptr[ndisks * num_blocks];
 #ifdef WATCH_TIMES
     double * r_finish_times = new double[ndisks];
     double * w_finish_times = new double[ndisks];
@@ -165,7 +165,7 @@ int main(int argc, char * argv[])
     double totaltimeread = 0, totaltimewrite = 0;
     stxxl::int64 totalsizeread = 0, totalsizewrite = 0;
 
-    for (i = 0; i < ndisks * buffer_size_int; i++)
+    for (i = 0; i < ndisks * step_size_int; i++)
         buffer[i] = i;
 
     for (i = 0; i < ndisks; i++)
@@ -190,14 +190,14 @@ int main(int argc, char * argv[])
     }
 
     std::cout << "# Buffer size: "
-              << buffer_size << " bytes per disk ("
-              << chunks << " chunks of "
-              << (buffer_size / chunks) << " bytes)" << std::endl;
+              << step_size << " bytes per disk ("
+              << num_blocks << " blocks of "
+              << (step_size / num_blocks) << " bytes)" << std::endl;
     try {
         while (!length || offset < endpos)
         {
-            const stxxl::int64 current_block_size = length ? std::min<stxxl::int64>(buffer_size, endpos - offset) : buffer_size;
-            const unsigned current_chunk_size = current_block_size / chunks;
+            const stxxl::int64 current_step_size = length ? std::min<stxxl::int64>(step_size, endpos - offset) : step_size;
+            const unsigned current_block_size = current_step_size / num_blocks;
 
             std::cout << "Disk offset " << std::setw(7) << offset / MB << " MB: " << std::fixed;
 
@@ -207,23 +207,23 @@ int main(int argc, char * argv[])
             if (do_write) {
             for (i = 0; i < ndisks; i++)
             {
-                for (j = 0; j < chunks; j++)
-                    reqs[i * chunks + j] =
-                        disks[i]->awrite(buffer + buffer_size_int * i + j * chunk_size_int,
-                                         offset + j * current_chunk_size,
-                                         current_chunk_size,
+                for (j = 0; j < num_blocks; j++)
+                    reqs[i * num_blocks + j] =
+                        disks[i]->awrite(buffer + step_size_int * i + j * block_size_int,
+                                         offset + j * current_block_size,
+                                         current_block_size,
                                          stxxl::default_completion_handler());
             }
 
  #ifdef WATCH_TIMES
             watch_times(reqs, ndisks, w_finish_times);
  #else
-            wait_all(reqs, ndisks * chunks);
+            wait_all(reqs, ndisks * num_blocks);
  #endif
 
             end = timestamp();
             elapsed = end - begin;
-            totalsizewrite += buffer_size;
+            totalsizewrite += step_size;
             totaltimewrite += elapsed;
             } else {
                 elapsed = 0.0;
@@ -232,9 +232,9 @@ int main(int argc, char * argv[])
 /*
    std::cout << "WRITE\nDisks: " << ndisks
         <<" \nElapsed time: "<< end-begin
-        << " \nThroughput: "<< int(1e-6*(buffer_size*ndisks)/(end-begin))
+        << " \nThroughput: "<< int(1e-6*(step_size*ndisks)/(end-begin))
         << " Mb/s \nPer one disk:"
-        << int(1e-6*(buffer_size)/(end-begin)) << " Mb/s"
+        << int(1e-6*(step_size)/(end-begin)) << " Mb/s"
         << std::endl;
 */
 
@@ -242,8 +242,8 @@ int main(int argc, char * argv[])
             out_stat(begin, end, w_finish_times, ndisks, disks_arr);
  #endif
             std::cout << std::setw(2) << ndisks << " * "
-                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_block_size) / elapsed) << " = "
-                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_block_size * ndisks) / elapsed) << " MB/s write,";
+                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_step_size) / elapsed) << " = "
+                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_step_size * ndisks) / elapsed) << " MB/s write,";
 #endif
 
 
@@ -253,22 +253,22 @@ int main(int argc, char * argv[])
             if (do_read) {
             for (i = 0; i < ndisks; i++)
             {
-                for (j = 0; j < chunks; j++)
-                    reqs[i * chunks + j] = disks[i]->aread(buffer + buffer_size_int * i + j * chunk_size_int,
-                                                           offset + j * current_chunk_size,
-                                                           current_chunk_size,
+                for (j = 0; j < num_blocks; j++)
+                    reqs[i * num_blocks + j] = disks[i]->aread(buffer + step_size_int * i + j * block_size_int,
+                                                           offset + j * current_block_size,
+                                                           current_block_size,
                                                            stxxl::default_completion_handler());
             }
 
  #ifdef WATCH_TIMES
             watch_times(reqs, ndisks, r_finish_times);
  #else
-            wait_all(reqs, ndisks * chunks);
+            wait_all(reqs, ndisks * num_blocks);
  #endif
 
             end = timestamp();
             elapsed = end - begin;
-            totalsizeread += buffer_size;
+            totalsizeread += step_size;
             totaltimeread += elapsed;
             } else {
                 elapsed = 0.0;
@@ -277,33 +277,33 @@ int main(int argc, char * argv[])
 /*
    std::cout << "READ\nDisks: " << ndisks
         <<" \nElapsed time: "<< end-begin
-        << " \nThroughput: "<< int(1e-6*(buffer_size*ndisks)/(end-begin))
+        << " \nThroughput: "<< int(1e-6*(step_size*ndisks)/(end-begin))
         << " Mb/s \nPer one disk:"
-        << int(1e-6*(buffer_size)/(end-begin)) << " Mb/s"
+        << int(1e-6*(step_size)/(end-begin)) << " Mb/s"
             << std::endl;
 */
 
             std::cout << std::setw(2) << ndisks << " * "
-                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_block_size) / elapsed) << " = "
-                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_block_size * ndisks) / elapsed) << " MB/s read" << std::endl;
+                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_step_size) / elapsed) << " = "
+                      << std::setw(7) << std::setprecision(3) << (1e-6 * (current_step_size * ndisks) / elapsed) << " MB/s read" << std::endl;
 
 #ifdef WATCH_TIMES
             out_stat(begin, end, r_finish_times, ndisks, disks_arr);
 #endif
 
             if (CHECK_AFTER_READ) {
-                for (i = 0; i < ndisks * buffer_size_int; i++)
+                for (i = 0; i < ndisks * step_size_int; i++)
                 {
                     if (buffer[i] != i)
                     {
-                        int ibuf = i / buffer_size_int;
-                        int pos = i % buffer_size_int;
+                        int ibuf = i / step_size_int;
+                        int pos = i % step_size_int;
 
                         std::cout << "Error on disk " << ibuf << " position " << std::hex << std::setw(8) << offset + pos * sizeof(int)
                                   << "  got: " << std::hex << std::setw(8) << buffer[i] << " wanted: " << std::hex << std::setw(8) << i
                                   << std::dec << std::endl;
 
-                        i = (ibuf + 1) * buffer_size_int; // jump to next
+                        i = (ibuf + 1) * step_size_int; // jump to next
                     }
                 }
             }
@@ -311,7 +311,7 @@ int main(int argc, char * argv[])
             std::cout << std::endl;
 #endif
 
-            offset += current_block_size;
+            offset += current_step_size;
         }
     }
     catch (const std::exception & ex)
