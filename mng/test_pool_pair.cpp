003fedfab5162227ec56686f3ca4fe66f0b3e401
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2009-05-13 13:02:16 +0000

    start a new test program for a pair of prefetch_pool and write_pool
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1699 4e380d45-d1fd-0310-85a7-d18ec86df0ad

---
+++ mng/test_pool_pair.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *  mng/test_prefetch_pool.cpp
+ *
+ *  Part of the STXXL. See http://stxxl.sourceforge.net
+ *
+ *  Copyright (C) 2003 Roman Dementiev <dementiev@mpi-sb.mpg.de>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************/
+
+//! \example mng/test_prefetch_pool.cpp
+
+#include <iostream>
+#include <stxxl/mng>
+#include <stxxl/bits/mng/prefetch_pool.h>
+
+#define BLOCK_SIZE (1024 * 512)
+
+struct MyType
+{
+    int integer;
+    char chars[5];
+};
+
+typedef stxxl::typed_block<BLOCK_SIZE, MyType> block_type;
+
+int main()
+{
+    stxxl::prefetch_pool<block_type> pool(2);
+    pool.resize(10);
+    pool.resize(5);
+
+    block_type * blk = new block_type;
+    (*blk)[0].integer = 42;
+    block_type::bid_type bids[2];
+    stxxl::block_manager::get_instance()->new_blocks(stxxl::single_disk(), bids, bids + 2);
+    blk->write(bids[0])->wait();
+    blk->write(bids[1])->wait();
+
+    pool.hint(bids[0]);
+    pool.read(blk, bids[0])->wait();
+    pool.read(blk, bids[1])->wait();
+
+    delete blk;
+}
