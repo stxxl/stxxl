7cc0155a6503896fe1139d826ce96858f7065984
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2009-09-13 13:37:15 +0000

    add another random_shuffle test to demonstrate a bug in write_vector helper
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@1844 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- algo/test_random_shuffle.cpp
+++ algo/test_random_shuffle.cpp
@@ -5,15 +5,20 @@
  *
  *  Copyright (C) 2007 Manuel Krings
  *  Copyright (C) 2007 Markus Westphal
+ *  Copyright (C) 2009 Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
  *
  *  Distributed under the Boost Software License, Version 1.0.
  *  (See accompanying file LICENSE_1_0.txt or copy at
  *  http://www.boost.org/LICENSE_1_0.txt)
  **************************************************************************/
 
+// TODO: test both vector and non-vector variant of random_shuffle
+// TODO: test recursion, improve verboseness
+
 //! \example algo/test_random_shuffle.cpp
 //! Test \c stxxl::random_shuffle()
 
+#include <stxxl/vector>
 #include <stxxl/random_shuffle>
 
 
@@ -30,8 +35,7 @@ struct counter
     }
 };
 
-
-int main()
+void long_test()
 {
     typedef stxxl::vector<int> ext_vec_type;
     ext_vec_type STXXLVector(1024 * 1024 * 256 / sizeof(int));
@@ -62,3 +66,37 @@ int main()
     for (i = STXXLVector.size() - 10; i < STXXLVector.size(); i++)
         STXXL_MSG(STXXLVector[i]);
 }
+
+void short_test()
+{
+    typedef stxxl::VECTOR_GENERATOR<int32_t, 1, 2, 4096>::result vector_type;
+    vector_type::size_type i;
+    vector_type v(2048);
+    for (i = 0; i < v.size(); ++i)
+        v[i] = i / 1024 + 1;
+
+    std::cout << v.size() << std::endl;
+    std::cout << "before shuffle:" << std::endl;
+    for (i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+        if ((i & 511) == 511)
+            std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    v.flush();
+    stxxl::random_shuffle(v.begin() + 512, v.begin() + 512 + 1024, 64 * 1024);
+    std::cout << "after shuffle:" << std::endl;
+    for (i = 0; i < v.size(); ++i) {
+        std::cout << v[i] << " ";
+        if ((i & 511) == 511)
+            std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    short_test();
+    long_test();
+}
+// vim: et:ts=4:sw=4
