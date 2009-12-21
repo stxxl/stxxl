deba1c299d701574a6fb76716ffa662777b31705
Johannes Singler <singler@ira.uka.de>
2009-12-21 09:29:52 +0000

    Take std::tr1::hash(_map) if GCC is new enough, but exclude Intel compiler.
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@2257 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/compat_hash_map.h
+++ include/stxxl/bits/compat_hash_map.h
@@ -20,6 +20,8 @@
  #include <unordered_map>
 #elif defined(BOOST_MSVC)
  #include <hash_map>
+#elif !defined(__INTEL_COMPILER) && ((__GNUC__ * 10000 + __GNUC_MINOR__ * 100) >= 40200)
+ #include <tr1/unordered_map>
 #else
  #include <ext/hash_map>
 #endif
@@ -35,6 +37,8 @@ struct compat_hash {
     typedef std::hash<_Tp> result;
 #elif defined(BOOST_MSVC)
     typedef stdext::hash_compare<_Tp> result;
+#elif !defined(__INTEL_COMPILER) && ((__GNUC__ * 10000 + __GNUC_MINOR__ * 100) >= 40200)
+    typedef std::tr1::hash<_Tp> result;
 #else
     typedef __gnu_cxx::hash<_Tp> result;
 #endif
@@ -46,6 +50,8 @@ struct compat_hash_map {
     typedef std::unordered_map<_Key, _Tp, _Hash> result;
 #elif defined(BOOST_MSVC)
     typedef stdext::hash_map<_Key, _Tp, _Hash> result;
+#elif !defined(__INTEL_COMPILER) && ((__GNUC__ * 10000 + __GNUC_MINOR__ * 100) >= 40200)
+    typedef std::tr1::unordered_map<_Key, _Tp, _Hash> result;
 #else
     typedef __gnu_cxx::hash_map<_Key, _Tp, _Hash> result;
 #endif
