b315611dcaf7b6159ac41a2a74538e23a1799712
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2010-07-19 12:05:41 +0000

    refactor stack_benchmark to duplicate less code
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@2451 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- containers/stack_benchmark.cpp
+++ containers/stack_benchmark.cpp
@@ -47,32 +47,22 @@ inline std::ostream & operator << (std::ostream & o, const my_record_<RECORD_SIZ
     return o;
 }
 
-template <class my_record>
-void run_stxxl_growshrink2_stack(stxxl::int64 volume)
+
+template <typename stack_type>
+void benchmark_insert(stack_type & Stack, stxxl::int64 volume)
 {
-    typedef typename stxxl::STACK_GENERATOR<my_record, stxxl::external,
-                                            stxxl::grow_shrink2, DISKS, BLOCK_SIZE>::result stack_type;
-    typedef typename stack_type::block_type block_type;
+    typedef typename stack_type::value_type value_type;
 
-    STXXL_MSG("Record size: " << sizeof(my_record) << " bytes");
+    stxxl::int64 ops = volume / sizeof(value_type);
 
-    stxxl::prefetch_pool<block_type> p_pool(DISKS * 4);
-    stxxl::write_pool<block_type> w_pool(DISKS * 4);
-
-    stack_type Stack(p_pool, w_pool);
-
-    stxxl::int64 ops = volume / sizeof(my_record);
-
-    stxxl::int64 i;
-
-    my_record cur;
+    value_type cur;
 
     stxxl::stats_data stats_begin(*stxxl::stats::get_instance());
 
     stxxl::timer Timer;
     Timer.start();
 
-    for (i = 0; i < ops; ++i)
+    for (stxxl::int64 i = 0; i < ops; ++i)
     {
         Stack.push(cur);
     }
@@ -80,7 +70,7 @@ void run_stxxl_growshrink2_stack(stxxl::int64 volume)
     Timer.stop();
 
     STXXL_MSG("Records in Stack: " << Stack.size());
-    if (i != Stack.size())
+    if (ops != Stack.size())
     {
         STXXL_MSG("Size does not match");
         abort();
@@ -91,16 +81,23 @@ void run_stxxl_growshrink2_stack(stxxl::int64 volume)
               " MiB/s");
 
     std::cout << stxxl::stats_data(*stxxl::stats::get_instance()) - stats_begin;
+}
 
-    Stack.set_prefetch_aggr(DISKS * 8);
+template <typename stack_type>
+void benchmark_delete(stack_type & Stack, stxxl::int64 volume)
+{
+    typedef typename stack_type::value_type value_type;
 
-    ////////////////////////////////////////////////
+    stxxl::int64 ops = volume / sizeof(value_type);
 
-    stats_begin = *stxxl::stats::get_instance();
-    Timer.reset();
+    value_type cur;
+
+    stxxl::stats_data stats_begin(*stxxl::stats::get_instance());
+
+    stxxl::timer Timer;
     Timer.start();
 
-    for (i = 0; i < ops; ++i)
+    for (stxxl::int64 i = 0; i < ops; ++i)
     {
         Stack.pop();
     }
@@ -123,6 +120,28 @@ void run_stxxl_growshrink2_stack(stxxl::int64 volume)
 
 
 template <class my_record>
+void run_stxxl_growshrink2_stack(stxxl::int64 volume)
+{
+    typedef typename stxxl::STACK_GENERATOR<my_record, stxxl::external,
+                                            stxxl::grow_shrink2, DISKS, BLOCK_SIZE>::result stack_type;
+    typedef typename stack_type::block_type block_type;
+
+    STXXL_MSG("Record size: " << sizeof(my_record) << " bytes");
+
+    stxxl::prefetch_pool<block_type> p_pool(DISKS * 4);
+    stxxl::write_pool<block_type> w_pool(DISKS * 4);
+
+    stack_type Stack(p_pool, w_pool);
+
+    benchmark_insert(Stack, volume);
+
+    Stack.set_prefetch_aggr(DISKS * 8);
+
+    benchmark_delete(Stack, volume);
+}
+
+
+template <class my_record>
 void run_stxxl_normal_stack(stxxl::int64 volume)
 {
     typedef typename stxxl::STACK_GENERATOR<my_record, stxxl::external,
@@ -133,62 +152,8 @@ void run_stxxl_normal_stack(stxxl::int64 volume)
 
     stack_type Stack;
 
-    stxxl::int64 ops = volume / sizeof(my_record);
-
-    stxxl::int64 i;
-
-    my_record cur;
-
-    stxxl::stats_data stats_begin(*stxxl::stats::get_instance());
-
-    stxxl::timer Timer;
-    Timer.start();
-
-    for (i = 0; i < ops; ++i)
-    {
-        Stack.push(cur);
-    }
-
-    Timer.stop();
-
-    STXXL_MSG("Records in Stack: " << Stack.size());
-    if (i != Stack.size())
-    {
-        STXXL_MSG("Size does not match");
-        abort();
-    }
-
-    STXXL_MSG("Insertions elapsed time: " << (Timer.mseconds() / 1000.) <<
-              " seconds : " << (double(volume) / (1024. * 1024. * Timer.mseconds() / 1000.)) <<
-              " MiB/s");
-
-    std::cout << stxxl::stats_data(*stxxl::stats::get_instance()) - stats_begin;
-
-    ////////////////////////////////////////////////
-
-    stats_begin = *stxxl::stats::get_instance();
-    Timer.reset();
-    Timer.start();
-
-    for (i = 0; i < ops; ++i)
-    {
-        Stack.pop();
-    }
-
-    Timer.stop();
-
-    STXXL_MSG("Records in Stack: " << Stack.size());
-    if (!Stack.empty())
-    {
-        STXXL_MSG("Stack must be empty");
-        abort();
-    }
-
-    STXXL_MSG("Deletions elapsed time: " << (Timer.mseconds() / 1000.) <<
-              " seconds : " << (double(volume) / (1024. * 1024. * Timer.mseconds() / 1000.)) <<
-              " MiB/s");
-
-    std::cout << stxxl::stats_data(*stxxl::stats::get_instance()) - stats_begin;
+    benchmark_insert(Stack, volume);
+    benchmark_delete(Stack, volume);
 }
 
 
@@ -204,15 +169,16 @@ int main(int argc, char * argv[])
 
     if (argc < 3)
     {
-        STXXL_MSG("Usage: " << argv[0] << " version #volume");
-        STXXL_MSG("\t version = 1: grow-shrink-stack2 with 4 byte records");
-        STXXL_MSG("\t version = 2: grow-shrink-stack2 with 32 byte records");
-        STXXL_MSG("\t version = 3: normal-stack with 4 byte records");
-        STXXL_MSG("\t version = 4: normal-stack with 32 byte records");
+        STXXL_MSG("Usage: " << argv[0] << " variant volume");
+        STXXL_MSG("\t variant = 1: grow-shrink-stack2 with 4 byte records");
+        STXXL_MSG("\t variant = 2: grow-shrink-stack2 with 32 byte records");
+        STXXL_MSG("\t variant = 3: normal-stack with 4 byte records");
+        STXXL_MSG("\t variant = 4: normal-stack with 32 byte records");
+        STXXL_MSG("\t volume:      in bytes");
         return -1;
     }
 
-    int version = atoi(argv[1]);
+    int variant = atoi(argv[1]);
     stxxl::int64 volume = stxxl::atoint64(argv[2]);
 
     STXXL_MSG("Allocating array with size " << MEM_2_RESERVE
@@ -220,10 +186,10 @@ int main(int argc, char * argv[])
     int * array = new int[MEM_2_RESERVE / sizeof(int)];
     std::fill(array, array + (MEM_2_RESERVE / sizeof(int)), 0);
 
-    STXXL_MSG("Running version: " << version);
+    STXXL_MSG("Running variant: " << variant);
     STXXL_MSG("Data volume    : " << volume << " bytes");
 
-    switch (version)
+    switch (variant)
     {
     case 1:
         run_stxxl_growshrink2_stack<my_record_<4> >(volume);
@@ -238,7 +204,7 @@ int main(int argc, char * argv[])
         run_stxxl_normal_stack<my_record_<32> >(volume);
         break;
     default:
-        STXXL_MSG("Unsupported version " << version);
+        STXXL_MSG("Unsupported variant " << variant);
     }
 
     delete[] array;
