3d48776674a9690a6b66074b6a963e84e87dbf85
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2010-07-19 18:22:53 +0000

    split streamop unique from stream.h
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@2468 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/stream/stream.h
+++ include/stxxl/bits/stream/stream.h
@@ -1808,111 +1808,14 @@ namespace stream
     };
 
 
-    ////////////////////////////////////////////////////////////////////////
-    //     UNIQUE                                                         //
-    ////////////////////////////////////////////////////////////////////////
-
-    //! \brief Equivalent to std::unique algorithms
-    //!
-    //! Removes consecutive duplicates from the stream.
-    //! Uses BinaryPredicate to compare elements of the stream
-    template <class Input, class BinaryPredicate = Stopper>
-    class unique
-    {
-        Input & input;
-        BinaryPredicate binary_pred;
-        typename Input::value_type current;
-
-    public:
-        //! \brief Standard stream typedef
-        typedef typename Input::value_type value_type;
-
-        unique(Input & input_, BinaryPredicate binary_pred_) : input(input_), binary_pred(binary_pred_)
-        {
-            if (!input.empty())
-                current = *input;
-        }
-
-        //! \brief Standard stream method
-        unique & operator ++ ()
-        {
-            value_type old_value = current;
-            ++input;
-            while (!input.empty() && (binary_pred(current = *input, old_value)))
-                ++input;
-        }
-
-        //! \brief Standard stream method
-        const value_type & operator * () const
-        {
-            return current;
-        }
-
-        //! \brief Standard stream method
-        const value_type * operator -> () const
-        {
-            return &current;
-        }
-
-        //! \brief Standard stream method
-        bool empty() const
-        {
-            return input.empty();
-        }
-    };
-
-    //! \brief Equivalent to std::unique algorithms
-    //!
-    //! Removes consecutive duplicates from the stream.
-    template <class Input>
-    class unique<Input, Stopper>
-    {
-        Input & input;
-        typename Input::value_type current;
-
-    public:
-        //! \brief Standard stream typedef
-        typedef typename Input::value_type value_type;
-
-        unique(Input & input_) : input(input_)
-        {
-            if (!input.empty())
-                current = *input;
-        }
-
-        //! \brief Standard stream method
-        unique & operator ++ ()
-        {
-            value_type old_value = current;
-            ++input;
-            while (!input.empty() && ((current = *input) == old_value))
-                ++input;
-        }
-
-        //! \brief Standard stream method
-        value_type & operator * () const
-        {
-            return current;
-        }
-
-        //! \brief Standard stream method
-        const value_type * operator -> () const
-        {
-            return &current;
-        }
-
-        //! \brief Standard stream method
-        bool empty() const
-        {
-            return input.empty();
-        }
-    };
-
-
 //! \}
 }
 
 __STXXL_END_NAMESPACE
+
+
+#include <stxxl/bits/stream/unique.h>
+
 
 #endif // !STXXL_STREAM_HEADER
 // vim: et:ts=4:sw=4
