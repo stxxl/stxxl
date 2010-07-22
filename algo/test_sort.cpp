9c0710ef2c0fa5d4e7491aaacce40e786d169d4b
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2010-07-22 12:04:33 +0000

    show in_memory_sort cache problem
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@2509 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- algo/test_sort.cpp
+++ algo/test_sort.cpp
@@ -87,6 +87,20 @@ int main()
 #endif
     unsigned memory_to_use = 128 * 1024 * 1024;
     typedef stxxl::vector<my_type> vector_type;
+
+    {
+        // test small vector that can be sorted internally
+        vector_type v(3);
+        v[0] = 42;
+        v[1] = 0;
+        v[2] = 23;
+        STXXL_MSG("small vector unsorted " << v[0] << " " << v[1] << " " << v[2]);
+        //stxxl::sort(v.begin(), v.end(), cmp(), memory_to_use);
+        stxxl::stl_in_memory_sort(v.begin(), v.end(), cmp());
+        STXXL_MSG("small vector sorted   " << v[0] << " " << v[1] << " " << v[2]);
+        assert(stxxl::is_sorted(v.begin(), v.end(), cmp()));
+    }
+
     const stxxl::int64 n_records =
         stxxl::int64(384) * stxxl::int64(1024 * 1024) / sizeof(my_type);
     vector_type v(n_records);
@@ -110,3 +124,5 @@ int main()
 
     return 0;
 }
+
+// vim: et:ts=4:sw=4
