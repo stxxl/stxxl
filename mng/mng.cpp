3e9103e201754449ca9f5dc9425bed25f3039164
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2010-10-30 14:40:51 +0000

    split mng/config.cpp from mng/mng.cpp
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@2709 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- mng/mng.cpp
+++ mng/mng.cpp
@@ -11,11 +11,8 @@
  *  http://www.boost.org/LICENSE_1_0.txt)
  **************************************************************************/
 
-#include <sstream>
-#include <stxxl/mng>
-#include <stxxl/bits/version.h>
+#include <stxxl/bits/mng/mng.h>
 #include <stxxl/bits/common/debug.h>
-#include <stxxl/bits/common/log.h>
 
 
 __STXXL_BEGIN_NAMESPACE
@@ -31,98 +28,6 @@ void DiskAllocator::dump()
         total += cur->second;
     }
     STXXL_ERRMSG("Total bytes: " << total);
-}
-
-
-void config::init(const char * config_path)
-{
-    logger::get_instance();
-    STXXL_MSG(get_version_string());
-    std::vector<DiskEntry> flash_props;
-    std::ifstream cfg_file(config_path);
-    if (!cfg_file)
-    {
-        STXXL_ERRMSG("Warning: no config file found.");
-        STXXL_ERRMSG("Using default disk configuration.");
-#ifndef BOOST_MSVC
-        DiskEntry entry1 = { "/var/tmp/stxxl", "syscall", 1000 * 1024 * 1024, true, false };
-#else
-        DiskEntry entry1 = { "", "wincall", 1000 * 1024 * 1024, true, false };
-        char * tmpstr = new char[255];
-        stxxl_check_ne_0(GetTempPath(255, tmpstr), resource_error);
-        entry1.path = tmpstr;
-        entry1.path += "stxxl";
-        delete[] tmpstr;
-#endif
-#if 0
-        DiskEntry entry2 =
-        { "/tmp/stxxl1", "mmap", 100 * 1024 * 1024, true };
-        DiskEntry entry3 =
-        { "/tmp/stxxl2", "simdisk", 1000 * 1024 * 1024, false };
-#endif
-        disks_props.push_back(entry1);
-        //disks_props.push_back(entry2);
-        //disks_props.push_back(entry3);
-    }
-    else
-    {
-        std::string line;
-
-        while (cfg_file >> line)
-        {
-            std::vector<std::string> tmp = split(line, "=");
-            bool is_disk;
-
-            if (tmp[0][0] == '#')
-            { }
-            else if ((is_disk = (tmp[0] == "disk")) || tmp[0] == "flash")
-            {
-                tmp = split(tmp[1], ",");
-                DiskEntry entry = {
-                    tmp[0], tmp[2],
-                    int64(atoi(tmp[1].c_str())) * int64(1024 * 1024),
-                    false,
-                    false
-                };
-                if (entry.size == 0)
-                    entry.autogrow = true;
-                if (is_disk)
-                    disks_props.push_back(entry);
-                else
-                    flash_props.push_back(entry);
-            }
-            else
-            {
-                std::cerr << "Unknown token " <<
-                tmp[0] << std::endl;
-            }
-        }
-        cfg_file.close();
-    }
-
-    // put flash devices after regular disks
-    first_flash = disks_props.size();
-    disks_props.insert(disks_props.end(), flash_props.begin(), flash_props.end());
-
-    if (disks_props.empty())
-        STXXL_THROW(std::runtime_error, "config::config", "No disks found in '" << config_path << "' .");
-
-#ifdef STXXL_VERBOSE_DISKS
-    for (std::vector<DiskEntry>::const_iterator it = disks_props.begin(); it != disks_props.end(); it++)
-    {
-        STXXL_MSG("Disk '" << (*it).path << "' is allocated, space: " <<
-                  ((*it).size) / (1024 * 1024) <<
-                  " MiB, I/O implementation: " << (*it).io_impl);
-    }
-#else
-    int64 total_size = 0;
-    for (std::vector<DiskEntry>::const_iterator it = disks_props.begin(); it != disks_props.end(); it++)
-        total_size += (*it).size;
-
-    STXXL_MSG("" << disks_props.size() << " disks are allocated, total space: " <<
-              (total_size / (1024 * 1024)) <<
-              " MiB");
-#endif
 }
 
 
