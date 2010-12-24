f3ab54abb6cf26ceaa934d924cecc6b8a5b8fcfc
Johannes Singler <singler@kit.edu>
2010-12-24 07:31:18 +0000

    Catch io_error exceptions for test_io_sizes.
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@2845 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- io/test_io_sizes.cpp
+++ io/test_io_sizes.cpp
@@ -31,52 +31,57 @@ int main(int argc, char ** argv)
     uint64 max_size = stxxl::atoint64(argv[3]);
     uint64 * buffer = (uint64 *)stxxl::aligned_alloc<4096>(max_size);
 
-    stxxl::file * file
-        = stxxl::create_file(argv[1], argv[2], stxxl::file::CREAT | stxxl::file::RDWR | stxxl::file::DIRECT);
-    file->set_size(max_size);
-
-    stxxl::request_ptr req;
-    stxxl::stats_data stats1(*stxxl::stats::get_instance());
-    for (uint64 size = 4096; size < max_size; size *= 2)
+    try
     {
-        //generate data
-        for (uint64 i = 0; i < size / sizeof(uint64); ++i)
-            buffer[i] = i;
+        std::auto_ptr<stxxl::file> file(
+            stxxl::create_file(argv[1], argv[2], stxxl::file::CREAT | stxxl::file::RDWR | stxxl::file::DIRECT));
+        file->set_size(max_size);
 
-        //write
-        STXXL_MSG(stxxl::add_IEC_binary_multiplier(size, "B") << "are being written at once");
-        req = file->awrite(buffer, 0, size, stxxl::default_completion_handler());
-        wait_all(&req, 1);
+        stxxl::request_ptr req;
+        stxxl::stats_data stats1(*stxxl::stats::get_instance());
+        for (uint64 size = 4096; size < max_size; size *= 2)
+        {
+            //generate data
+            for (uint64 i = 0; i < size / sizeof(uint64); ++i)
+                buffer[i] = i;
 
-        //fill with wrong data
-        for (uint64 i = 0; i < size / sizeof(uint64); ++i)
-            buffer[i] = 0xFFFFFFFFFFFFFFFFull;
+            //write
+            STXXL_MSG(stxxl::add_IEC_binary_multiplier(size, "B") << "are being written at once");
+            req = file->awrite(buffer, 0, size, stxxl::default_completion_handler());
+            wait_all(&req, 1);
 
-        //read again
-        STXXL_MSG(stxxl::add_IEC_binary_multiplier(size, "B") << "are being read at once");
-        req = file->aread(buffer, 0, size, stxxl::default_completion_handler());
-        wait_all(&req, 1);
+            //fill with wrong data
+            for (uint64 i = 0; i < size / sizeof(uint64); ++i)
+                buffer[i] = 0xFFFFFFFFFFFFFFFFull;
 
-        //check
-        bool wrong = false;
-        for (uint64 i = 0; i < size / sizeof(uint64); ++i)
-            if (buffer[i] != i)
-            {
-                STXXL_ERRMSG("Read inconsistent data at position " << i * sizeof(uint64));
-                wrong = true;
+            //read again
+            STXXL_MSG(stxxl::add_IEC_binary_multiplier(size, "B") << "are being read at once");
+            req = file->aread(buffer, 0, size, stxxl::default_completion_handler());
+            wait_all(&req, 1);
+
+            //check
+            bool wrong = false;
+            for (uint64 i = 0; i < size / sizeof(uint64); ++i)
+                if (buffer[i] != i)
+                {
+                    STXXL_ERRMSG("Read inconsistent data at position " << i * sizeof(uint64));
+                    wrong = true;
+                    break;
+                }
+
+            if (wrong)
                 break;
-            }
+        }
+        std::cout << stxxl::stats_data(*stxxl::stats::get_instance()) - stats1;
 
-        if (wrong)
-            break;
+        file->remove();
     }
-    std::cout << stxxl::stats_data(*stxxl::stats::get_instance()) - stats1;
+    catch(stxxl::io_error e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
 
     stxxl::aligned_dealloc<4096>(buffer);
-
-    file->remove();
-
-    delete file;
 
     return 0;
 }
