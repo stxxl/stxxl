1f8f89253f0a1b3490b263aebc1899bab3fb6784
Raoul Steffen <R-Steffen@gmx.de>
2011-02-12 05:51:50 +0000

    block_scheduler got it’s own include-file
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/branches/matrix@2959 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- containers/test_matrix.cpp
+++ containers/test_matrix.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
- *  include/stxxl/bits/containers/test_matrix.cpp
+ *  containers/test_matrix.cpp
  *
  *  Part of the STXXL. See http://stxxl.sourceforge.net
  *
- *  Copyright (C) 2010 Raoul Steffen <R-Steffen@gmx.de>
+ *  Copyright (C) 2010-2011 Raoul Steffen <R-Steffen@gmx.de>
  *
  *  Distributed under the Boost Software License, Version 1.0.
  *  (See accompanying file LICENSE_1_0.txt or copy at
