c8b448a729cfffea350864cf8533c1bd244adad0
Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
2011-03-08 13:19:25 +0000

    add a specialization lru_pager<0> which sets the size at run time, not compile time, previously stxxl::btree::pager
    git-svn-id: svn://svn.code.sf.net/p/stxxl/code/trunk@3032 4e380d45-d1fd-0310-85a7-d18ec86df0ad

--- include/stxxl/bits/containers/pager.h
+++ include/stxxl/bits/containers/pager.h
@@ -3,7 +3,7 @@
  *
  *  Part of the STXXL. See http://stxxl.sourceforge.net
  *
- *  Copyright (C) 2002-2003 Roman Dementiev <dementiev@mpi-sb.mpg.de>
+ *  Copyright (C) 2002, 2003, 2006 Roman Dementiev <dementiev@ira.uka.de>
  *  Copyright (C) 2011 Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
  *
  *  Distributed under the Boost Software License, Version 1.0.
@@ -55,7 +55,7 @@ public:
 };
 
 //! \brief Pager with \b LRU replacement strategy
-template <unsigned npages_>
+template <unsigned npages_ = 0>
 class lru_pager : private noncopyable
 {
     typedef unsigned_type size_type;
@@ -88,6 +88,45 @@ public:
 
     void swap(lru_pager & obj)
     {
+        std::swap(history, obj.history);
+        std::swap(history_entry, obj.history_entry);
+    }
+};
+
+// specialization, size is specified at runtime
+template <>
+class lru_pager<0> : private noncopyable
+{
+    typedef unsigned_type size_type;
+    typedef std::list<size_type> list_type;
+
+    size_type n_pages;
+    list_type history;
+    simple_vector<list_type::iterator> history_entry;
+
+public:
+    lru_pager(size_type npages_ = 0) : n_pages(npages_), history_entry(n_pages)
+    {
+        for (size_type i = 0; i < n_pages; ++i)
+            history_entry[i] = history.insert(history.end(), i);
+    }
+
+    ~lru_pager() { }
+
+    size_type kick()
+    {
+        return history.back();
+    }
+
+    void hit(size_type ipage)
+    {
+        assert(ipage < n_pages);
+        history.splice(history.begin(), history, history_entry[ipage]);
+    }
+
+    void swap(lru_pager & obj)
+    {
+        std::swap(n_pages, obj.n_pages);
         std::swap(history, obj.history);
         std::swap(history_entry, obj.history_entry);
     }
