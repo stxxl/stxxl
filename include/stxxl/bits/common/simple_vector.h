ea4d9ba89c7f1d9d0dda72f4d968dc241a0179d7
Timo Bingmann <tbgit@panthema.net>
2013-09-20 13:10:47 +0200

    Writing documentation about common utilities.

--- include/stxxl/bits/common/simple_vector.h
+++ include/stxxl/bits/common/simple_vector.h
@@ -5,6 +5,7 @@
  *
  *  Copyright (C) 2002 Roman Dementiev <dementiev@mpi-sb.mpg.de>
  *  Copyright (C) 2008, 2011 Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
+ *  Copyright (C) 2013 Timo Bingmann <tb@panthema.net>
  *
  *  Distributed under the Boost Software License, Version 1.0.
  *  (See accompanying file LICENSE_1_0.txt or copy at
@@ -21,47 +22,71 @@
 
 __STXXL_BEGIN_NAMESPACE
 
-template <class _Tp /*, class _Alloc=__STL_DEFAULT_ALLOCATOR(_Tp) */>
+/*!
+ * \brief Simpler non-growing vector without initialization.
+ *
+ * simple_vector can be used a replacement for std::vector when only a
+ * non-growing array is needed. The advantages of simple_vector are that it
+ * does not initilize memory for POD types (faster), allows simpler inlines and
+ * is less error prone to copying and other problems..
+ */
+template <class Type /*, class Alloc=__STL_DEFAULT_ALLOCATOR(Type) */>
 class simple_vector : private noncopyable
 {
 public:
     typedef unsigned_type size_type;
-    typedef _Tp value_type;
-//  typedef simple_alloc<_Tp, _Alloc> _data_allocator;
+    typedef Type value_type;
+//  typedef simple_alloc<Type, Alloc> _data_allocator;
 
 protected:
-    size_type _size;
-    value_type * _array;
+    //! size of allocated memory
+    size_type m_size;
+
+    //! pointer to allocated memory area
+    value_type* m_array;
 
 public:
-    typedef value_type * iterator;
-    typedef const value_type * const_iterator;
-    typedef value_type & reference;
-    typedef const value_type & const_reference;
+    // *** simple pointer iterators
 
-    simple_vector(size_type sz) : _size(sz), _array(NULL)
+    typedef value_type* iterator;
+    typedef const value_type* const_iterator;
+    typedef value_type& reference;
+    typedef const value_type& const_reference;
+
+public:
+    /// allocate vector's memory
+    simple_vector(size_type sz)
+        : m_size(sz), m_array(NULL)
     {
-        //    _array = _data_allocator.allocate(sz);
-        if (size() > 0)
-            _array = new _Tp[size()];
+        // m_array = _data_allocator.allocate(sz);
+        if (m_size > 0)
+            m_array = new Type[m_size];
     }
     void swap(simple_vector & obj)
     {
-        std::swap(_size, obj._size);
-        std::swap(_array, obj._array);
+        std::swap(m_size, obj.m_size);
+        std::swap(m_array, obj.m_array);
     }
     ~simple_vector()
     {
-        //    _data_allocator.deallocate(_array,_size);
-        delete[] _array;
+        // _data_allocator.deallocate(m_array,m_size);
+        delete[] m_array;
+    }
+    iterator data()
+    {
+        return m_array;
+    }
+    const_iterator data() const
+    {
+        return m_array;
     }
     iterator begin()
     {
-        return _array;
+        return m_array;
     }
     const_iterator begin() const
     {
-        return _array;
+        return m_array;
     }
     const_iterator cbegin() const
     {
@@ -69,11 +94,11 @@ public:
     }
     iterator end()
     {
-        return _array + _size;
+        return m_array + m_size;
     }
     const_iterator end() const
     {
-        return _array + _size;
+        return m_array + m_size;
     }
     const_iterator cend() const
     {
@@ -81,7 +106,7 @@ public:
     }
     size_type size() const
     {
-        return _size;
+        return m_size;
     }
     reference operator [] (size_type i)
     {
