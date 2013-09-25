5003bc4201e171effc1be5b44894315518a169b4
Timo Bingmann <tbgit@panthema.net>
2013-09-25 14:30:36 +0200

    Adding uint40 and uint48 integers with 40- and 48-bits, with test case.

---
+++ tests/common/test_uint_types.cpp
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *  tests/common/test_uint_types.cpp
+ *
+ *  Part of the STXXL. See http://stxxl.sourceforge.net
+ *
+ *  Copyright (C) 2013 Timo Bingmann <tb@panthema.net>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************/
+
+#include <stxxl/bits/common/uint_types.h>
+#include <stxxl/bits/verbose.h>
+
+// forced instantiation
+template class stxxl::uint_pair<uint8_t>;
+template class stxxl::uint_pair<uint16_t>;
+
+template <typename uint>
+void dotest()
+{
+    // simple initialize
+    uint a = 42;
+
+    // count up 1024 and down again
+    uint b = 0xFFFFFF00llu;
+    uint b_save = b; 
+
+    uint64_t b64 = b;
+    for (uint32_t i = 0; i < 1024; ++i)
+    {
+        STXXL_CHECK( b.u64() == b64 );
+        STXXL_CHECK( b.ull() == b64 );
+        STXXL_CHECK( b != a );
+        ++b, ++b64;
+    }
+
+    STXXL_CHECK( b != b_save );
+
+    for (uint32_t i = 0; i < 1024; ++i)
+    {
+        STXXL_CHECK( b.u64() == b64 );
+        STXXL_CHECK( b.ull() == b64 );
+        STXXL_CHECK( b != a );
+        --b, --b64;
+    }
+
+    STXXL_CHECK( b.u64() == b64 );
+    STXXL_CHECK( b.ull() == b64 );
+    STXXL_CHECK( b == b_save );
+
+    // check min and max value
+    STXXL_CHECK( uint::min() <= a );
+    STXXL_CHECK( uint::max() >= a );
+
+    STXXL_CHECK( std::numeric_limits<uint>::min() < a );
+    STXXL_CHECK( std::numeric_limits<uint>::max() > a );
+
+    // check simple math
+    a = 42;
+    a = a + a;
+    STXXL_CHECK( a == uint(84) );
+    STXXL_CHECK( a.ull() == 84 );
+
+    a += uint(0xFFFFFF00llu);
+    STXXL_CHECK( a.ull() == 84 + 0xFFFFFF00llu );
+}
+
+int main()
+{
+    dotest<stxxl::uint40>();
+    dotest<stxxl::uint48>();
+
+    return 0;
+}
