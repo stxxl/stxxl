3f5eb45dd98f756e84388fbc456e021d24c6c947
Timo Bingmann <tbgit@panthema.net>
2013-10-09 00:00:52 +0200

    Not building examples in default cmake config, added stxxl_build_example() macro to conditionally build them.

--- examples/containers/stack1.cpp
+++ examples/containers/stack1.cpp
@@ -16,7 +16,7 @@
 int main()
 {
     // template parameter <data_type, externality, behaviour, blocks_per_page, block_size, internal_stack_type, migrating_critical_size, allocation_strategy, size_type>
-    typedef stxxl::STACK_GENERATOR<int, stxxl::external, stxxl::grow_shrink_stack2, 4, 2*1024*1024>::result simple_stack;
+    typedef stxxl::STACK_GENERATOR<int, stxxl::external, stxxl::grow_shrink, 4, 2*1024*1024>::result simple_stack;
 
     // create stack instance
     simple_stack a_stack;
@@ -27,13 +27,13 @@ int main()
     // routine: 1) push random values on stack and 2) pop all except the lowest value and start again
     for (int k = 0; k < 5; k++) {
         STXXL_MSG("push...");
-        for (stxxl::int64 i = 0; i < number_of_elements; i++)
+        for (stxxl::uint64 i = 0; i < number_of_elements; i++)
         {
             a_stack.push(random(123456789));
         }
 
         STXXL_MSG("pop...");
-        for (stxxl::int64 j = 0; j < number_of_elements - 1; j++)
+        for (stxxl::uint64 j = 0; j < number_of_elements - 1; j++)
         {
             a_stack.pop();
         }
