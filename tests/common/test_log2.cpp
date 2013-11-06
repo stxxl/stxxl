5d6670acf66b7c862e43a0b703ce9cbbfa7e383e
Timo Bingmann <tbgit@panthema.net>
2013-11-06 20:50:13 +0100

    Simplifying test_log2 to not use floorl and similar.

--- tests/common/test_log2.cpp
+++ tests/common/test_log2.cpp
@@ -23,41 +23,47 @@ using stxxl::LOG2;
 using stxxl::unsigned_type;
 
 template <unsigned_type i>
-void log_i()
+void log_i(unsigned_type floorv, unsigned_type ceilv)
 {
     std::cout << i << "\t" << (i < 1000000 ? "\t" : "")
               << stxxl::LOG2_floor<i>::value << "\t"
               << stxxl::LOG2<i>::floor << "\t"
               << stxxl::LOG2<i>::ceil << std::endl;
 
-#if !STXXL_MSVC && !defined(__CYGWIN__)
     std::cout << "\t\t"
-              << log2l((long double)i) << "\t"
-              << (unsigned_type)floorl(log2l(i)) << "\t"
-              << (unsigned_type)ceill(log2l(i)) << "\n";
-#endif
+              << stxxl::ilog2_floor(i) << "\t"
+              << stxxl::ilog2_floor(i) << "\t"
+              << stxxl::ilog2_ceil(i) << std::endl;
 
-    if (i == 0)
+    STXXL_CHECK( stxxl::LOG2_floor<i>::value == stxxl::ilog2_floor(i) );
+    STXXL_CHECK( stxxl::LOG2<i>::floor == stxxl::ilog2_floor(i) );
+    STXXL_CHECK( stxxl::LOG2<i>::ceil == stxxl::ilog2_ceil(i) );
+
+    if (i <= 1)
     {
         STXXL_CHECK( stxxl::LOG2_floor<i>::value == 0 );
         STXXL_CHECK( stxxl::LOG2<i>::floor == 0 );
         STXXL_CHECK( stxxl::LOG2<i>::ceil == 0 );
     }
-    else if (i <= ((stxxl::uint64)(1) << 59)) // does not work for higher powers
+    else if (i == 2)
     {
-        std::cout << "\t\t"
-                  << stxxl::ilog2_floor(i) << "\t"
-                  << stxxl::ilog2_floor(i) << "\t"
-                  << stxxl::ilog2_ceil(i) << "\n";
+        STXXL_CHECK( stxxl::LOG2_floor<i>::value == 1 );
+        STXXL_CHECK( stxxl::LOG2<i>::floor == 1 );
+        STXXL_CHECK( stxxl::LOG2<i>::ceil == 1 );
+    }
+    else
+    {
+        STXXL_CHECK( stxxl::LOG2_floor<i>::value == floorv );
+        STXXL_CHECK( stxxl::LOG2<i>::floor == floorv );
+        STXXL_CHECK( stxxl::LOG2<i>::ceil == ceilv );
 
-        STXXL_CHECK( stxxl::LOG2_floor<i>::value == stxxl::ilog2_floor(i) );
-        STXXL_CHECK( stxxl::LOG2<i>::floor == stxxl::ilog2_floor(i) );
-        STXXL_CHECK( stxxl::LOG2<i>::ceil == stxxl::ilog2_ceil(i) );
-
-#if !STXXL_MSVC && !defined(__CYGWIN__)
-        STXXL_CHECK( stxxl::LOG2_floor<i>::value == (unsigned_type)floorl(log2l(i)) );
-        STXXL_CHECK( stxxl::LOG2<i>::floor == (unsigned_type)floorl(log2l(i)) );
-        STXXL_CHECK( stxxl::LOG2<i>::ceil == (unsigned_type)ceill(log2l(i)) );
+#if 0 // not many compiler have log2l()
+        if (i <= ((stxxl::uint64)(1) << 59)) // does not work for higher powers
+        {
+            STXXL_CHECK( stxxl::LOG2_floor<i>::value == (unsigned_type)floorl(log2l(i)) );
+            STXXL_CHECK( stxxl::LOG2<i>::floor == (unsigned_type)floorl(log2l(i)) );
+            STXXL_CHECK( stxxl::LOG2<i>::ceil == (unsigned_type)ceill(log2l(i)) );
+        }
 #endif
     }
 
@@ -65,11 +71,11 @@ void log_i()
 }
 
 template <unsigned_type i>
-void log_ipm1()
+void log_ipm1(unsigned_type p)
 {
-    log_i<i - 1>();
-    log_i<i>();
-    log_i<i + 1>();
+    log_i<i - 1>(p - 1, p);
+    log_i<i>(p, p);
+    log_i<i + 1>(p, p + 1);
     std::cout << std::endl;
 }
 
@@ -77,29 +83,29 @@ int main()
 {
     std::cout << "i\t\tLOG2<i>\tLOG2<i>\tLOG2<i>" << std::endl;
     std::cout << "\t\t\tfloor\tceil" << std::endl;
-    log_ipm1<1 << 0>();
-    log_ipm1<1 << 1>();
-    log_ipm1<1 << 2>();
-    log_ipm1<1 << 3>();
-    log_ipm1<1 << 4>();
-    log_ipm1<1 << 5>();
-    log_ipm1<1 << 6>();
-    log_ipm1<1 << 7>();
-    log_ipm1<1 << 8>();
-    log_ipm1<1 << 9>();
-    log_ipm1<1 << 10>();
-    log_ipm1<1 << 11>();
-    log_ipm1<1 << 12>();
-    log_ipm1<1 << 16>();
-    log_ipm1<1 << 24>();
-    log_ipm1<1 << 30>();
-    log_ipm1<1UL << 31>();
+    log_ipm1<1 << 0>(0);
+    log_ipm1<1 << 1>(1);
+    log_ipm1<1 << 2>(2);
+    log_ipm1<1 << 3>(3);
+    log_ipm1<1 << 4>(4);
+    log_ipm1<1 << 5>(5);
+    log_ipm1<1 << 6>(6);
+    log_ipm1<1 << 7>(7);
+    log_ipm1<1 << 8>(8);
+    log_ipm1<1 << 9>(9);
+    log_ipm1<1 << 10>(10);
+    log_ipm1<1 << 11>(11);
+    log_ipm1<1 << 12>(12);
+    log_ipm1<1 << 16>(16);
+    log_ipm1<1 << 24>(24);
+    log_ipm1<1 << 30>(30);
+    log_ipm1<1UL << 31>(31);
 #if __WORDSIZE == 64
-    log_ipm1<1UL << 32>();
-    log_ipm1<1UL << 33>();
-    log_ipm1<1UL << 48>();
-    log_ipm1<1UL << 50>();
-    log_ipm1<1UL << 55>();
-    log_ipm1<1UL << 63>();
+    log_ipm1<1UL << 32>(32);
+    log_ipm1<1UL << 33>(33);
+    log_ipm1<1UL << 48>(48);
+    log_ipm1<1UL << 50>(50);
+    log_ipm1<1UL << 55>(55);
+    log_ipm1<1UL << 63>(63);
 #endif
 }
