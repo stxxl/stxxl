78f26262f1ae971e5f0d7c48723d293192a32ed3
Timo Bingmann <tbgit@panthema.net>
2013-11-06 23:06:31 +0100

    Extracting large functions from common/cmdline.h into lib/common/cmdline.cpp

--- include/stxxl/bits/common/cmdline.h
+++ include/stxxl/bits/common/cmdline.h
@@ -349,46 +349,10 @@ public:
     //! Wrap a long string at spaces into lines. Prefix is added
     //! unconditionally to each line. Lines are wrapped after wraplen
     //! characters if possible.
-    static inline void
+    static void
     output_wrap(std::ostream& os, const std::string& text, size_t wraplen,
                 size_t indent_first = 0, size_t indent_rest = 0,
-                size_t current = 0, size_t indent_newline = 0)
-    {
-        std::string::size_type t = 0;
-        size_t indent = indent_first;
-        while (t != text.size())
-        {
-            std::string::size_type to = t, lspace = t;
-
-            // scan forward in text until we hit a newline or wrap point
-            while (to != text.size() &&
-                   to + current + indent < t + wraplen &&
-                   text[to] != '\n')
-            {
-                if (text[to] == ' ') lspace = to;
-                ++to;
-            }
-
-            // go back to last space
-            if (to != text.size() && text[to] != '\n' &&
-                lspace != t) to = lspace+1;
-
-            // output line
-            os << std::string(indent, ' ')
-               << text.substr(t, to - t) << std::endl;
-
-            current = 0;
-            indent = indent_rest;
-
-            // skip over last newline
-            if (to != text.size() && text[to] == '\n') {
-                indent = indent_newline;
-                ++to;
-            }
-
-            t = to;
-        }
-    }
+                size_t current = 0, size_t indent_newline = 0);
 
 public:
 
@@ -612,305 +576,26 @@ public:
 
     //! output nicely formatted usage information including description of all
     //! parameters and options.
-    void print_usage(std::ostream& os = std::cout)
-    {
-        std::ios state(NULL);
-        state.copyfmt(os);
-
-        os << "Usage: " << m_progname
-           << (m_optlist.size() ? " [options]" : "");
-
-        for (arglist_type::const_iterator it = m_paramlist.begin();
-             it != m_paramlist.end(); ++it)
-        {
-            const argument* arg = *it;
-
-            os << (arg->m_required ? " <" : " [")
-               << arg->m_longkey
-               << (arg->m_repeated ? " ..." : "")
-               << (arg->m_required ? '>' : ']');
-        }
-
-        os << std::endl;
-
-        if (m_description.size())
-        {
-            os << std::endl;
-            output_wrap(os, m_description, m_linewrap);
-        }
-        if (m_author.size())
-        {
-            os << "Author: " << m_author << std::endl;
-        }
-
-        if (m_description.size() || m_author.size())
-            os << std::endl;
-
-        if (m_paramlist.size())
-        {
-            os << "Parameters:" << std::endl;
-
-            for (arglist_type::const_iterator it = m_paramlist.begin();
-                 it != m_paramlist.end(); ++it)
-            {
-                const argument* arg = *it;
-
-                os << "  " << std::setw(m_param_maxlong) << std::left << arg->param_text();
-                output_wrap(os, arg->m_desc, m_linewrap,
-                            0, m_param_maxlong+2, m_param_maxlong+2, 8);
-            }
-        }
-
-        if (m_optlist.size())
-        {
-            os << "Options:" << std::endl;
-
-            for (arglist_type::const_iterator it = m_optlist.begin();
-                 it != m_optlist.end(); ++it)
-            {
-                const argument* arg = *it;
-
-                os << "  " << std::setw(m_opt_maxlong) << std::left << arg->option_text();
-                output_wrap(os, arg->m_desc, m_linewrap,
-                            0, m_opt_maxlong+2, m_opt_maxlong+2, 8);
-            }
-        }
-
-        os.copyfmt(state);
-    }
+    void print_usage(std::ostream& os = std::cout);
 
 private:
 
     //! print error about option.
     void print_option_error(int argc, const char* const* argv, const argument* arg,
-                            std::ostream& os)
-    {
-        os << "Error: Argument ";
-        if (argc != 0)
-            os << '"' << argv[0] << '"';
-
-        os << " for " << arg->type_name() << " option " << arg->option_text()
-           << (argc == 0 ? " is missing!" : " is invalid!")
-           << std::endl << std::endl;
-
-        print_usage(os);
-    }
+                            std::ostream& os);
 
     //! print error about parameter.
     void print_param_error(int argc, const char* const* argv, const argument* arg,
-                           std::ostream& os)
-    {
-        os << "Error: Argument ";
-        if (argc != 0)
-            os << '"' << argv[0] << '"';
-
-        os << " for " << arg->type_name() << " parameter " << arg->param_text()
-           << (argc == 0 ? " is missing!" : " is invalid!")
-           << std::endl << std::endl;
-
-        print_usage(os);
-    }
+                           std::ostream& os);
 
 public:
 
     //! parse command line options as specified by the options and parameters added.
     //! \return true if command line is okay and all required parameters are present.
-    bool process(int argc, const char* const* argv, std::ostream& os = std::cout)
-    {
-        m_progname = argv[0];
-        --argc, ++argv;
-
-        // search for help string and output help
-        for (int i = 0; i < argc; ++i)
-        {
-            if (strcmp(argv[i], "-h") == 0 ||
-                strcmp(argv[i], "--help") == 0)
-            {
-                print_usage(os);
-                return false;
-            }
-        }
-
-        arglist_type::iterator argi = m_paramlist.begin(); // current argument in m_paramlist
-        bool end_optlist = false;
-
-        while (argc != 0)
-        {
-            const char* arg = argv[0];
-
-            if (arg[0] == '-' && !end_optlist)
-            {
-                // option, advance to argument
-                --argc, ++argv;
-                if (arg[1] == '-')
-                {
-                    if (arg[2] == '-') {
-                        end_optlist = true;
-                    }
-                    else {
-                        // long option
-                        arglist_type::const_iterator oi = m_optlist.begin();
-                        for ( ; oi != m_optlist.end(); ++oi)
-                        {
-                            if ((arg+2) == (*oi)->m_longkey)
-                            {
-                                if (!(*oi)->process(argc, argv))
-                                {
-                                    print_option_error(argc, argv, *oi, os);
-                                    return false;
-                                }
-                                else if (m_verbose_process)
-                                {
-                                    os << "Option " << (*oi)->option_text() << " set to ";
-                                    (*oi)->print_value(os);
-                                    os << '.' << std::endl;
-                                }
-                                break;
-                            }
-                        }
-                        if (oi == m_optlist.end())
-                        {
-                            os << "Error: Unknown option \"" << arg << "\"." << std::endl << std::endl;
-                            print_usage(os);
-                            return false;
-                        }
-                    }
-                }
-                else
-                {
-                    // short option
-                    if (arg[1] == 0) {
-                        os << "Invalid option \"" << arg << '"' << std::endl;
-                    }
-                    else {
-                        arglist_type::const_iterator oi = m_optlist.begin();
-                        for ( ; oi != m_optlist.end(); ++oi)
-                        {
-                            if (arg[1] == (*oi)->m_key)
-                            {
-                                if (!(*oi)->process(argc, argv))
-                                {
-                                    print_option_error(argc, argv, *oi, os);
-                                    return false;
-                                }
-                                else if (m_verbose_process)
-                                {
-                                    os << "Option " << (*oi)->option_text() << " set to ";
-                                    (*oi)->print_value(os);
-                                    os << '.' << std::endl;
-                                }
-                                break;
-                            }
-                        }
-                        if (oi == m_optlist.end())
-                        {
-                            os << "Error: Unknown option \"" << arg << "\"." << std::endl << std::endl;
-                            print_usage(os);
-                            return false;
-                        }
-                    }
-                }
-            }
-            else
-            {
-                if (argi != m_paramlist.end())
-                {
-                    if (!(*argi)->process(argc, argv))
-                    {
-                        print_param_error(argc, argv, *argi, os);
-                        return false;
-                    }
-                    else if (m_verbose_process)
-                    {
-                        os << "Parameter " << (*argi)->param_text() << " set to ";
-                        (*argi)->print_value(os);
-                        os << '.' << std::endl;
-                    }
-                    (*argi)->m_found = true;
-                    if (!(*argi)->m_repeated)
-                        ++argi;
-                }
-                else
-                {
-                    os << "Error: Unexpected extra argument \"" << argv[0] << "\"."
-                       << std::endl << std::endl;
-                    --argc, ++argv;
-                    print_usage(os);
-                    return false;
-                }
-            }
-        }
-
-        bool good = true;
-
-        for (arglist_type::const_iterator it = m_paramlist.begin();
-             it != m_paramlist.end(); ++it)
-        {
-            if ((*it)->m_required && !(*it)->m_found) {
-                os << "Error: Argument for parameter "
-                   << (*it)->m_longkey << " is required!" << std::endl;
-                good = false;
-            }
-        }
-
-        if (!good) {
-            os << std::endl;
-            print_usage(os);
-        }
-
-        return good;
-    }
+    bool process(int argc, const char* const* argv, std::ostream& os = std::cout);
 
     //! print nicely formatted result of processing
-    void print_result(std::ostream& os = std::cout)
-    {
-        std::ios state(NULL);
-        state.copyfmt(os);
-
-        size_t maxlong = STXXL_MAX(m_param_maxlong, m_opt_maxlong);
-
-        if (m_paramlist.size())
-        {
-            os << "Parameters:" << std::endl;
-
-            for (arglist_type::const_iterator it = m_paramlist.begin();
-                 it != m_paramlist.end(); ++it)
-            {
-                const argument* arg = *it;
-
-                os << "  " << std::setw(maxlong) << std::left << arg->param_text();
-
-                std::string typestr = "(" + std::string(arg->type_name()) + ")";
-                os << std::setw(m_maxtypename+4) << typestr;
-
-                arg->print_value(os);
-
-                os << std::endl;
-            }
-        }
-
-        if (m_optlist.size())
-        {
-            os << "Options:" << std::endl;
-
-            for (arglist_type::const_iterator it = m_optlist.begin();
-                 it != m_optlist.end(); ++it)
-            {
-                const argument* arg = *it;
-
-                os << "  " << std::setw(maxlong) << std::left << arg->option_text();
-
-                std::string typestr = "(" + std::string(arg->type_name()) + ")";
-                os << std::setw(m_maxtypename+4) << std::left << typestr;
-
-                arg->print_value(os);
-
-                os << std::endl;
-            }
-        }
-
-        os.copyfmt(state);
-    }
+    void print_result(std::ostream& os = std::cout);
 };
 
 __STXXL_END_NAMESPACE
