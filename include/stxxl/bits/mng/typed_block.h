6317b5cd082c240436bed458390267a7e3b0537c
Timo Bingmann <tbgit@panthema.net>
2014-09-26 18:22:58 +0200

    Make typed_block::new less verbose in VERBOSE1.

--- include/stxxl/bits/mng/typed_block.h
+++ include/stxxl/bits/mng/typed_block.h
@@ -302,7 +302,7 @@ public:
     static void* operator new (size_t bytes)
     {
         unsigned_type meta_info_size = bytes % raw_size;
-        STXXL_VERBOSE1("typed::block operator new[]: bytes=" << bytes << ", meta_info_size=" << meta_info_size);
+        STXXL_VERBOSE_TYPED_BLOCK("typed::block operator new[]: bytes=" << bytes << ", meta_info_size=" << meta_info_size);
 
         void* result = aligned_alloc<STXXL_BLOCK_ALIGN>(
             bytes - meta_info_size, meta_info_size);
@@ -316,7 +316,7 @@ public:
     static void* operator new[] (size_t bytes)
     {
         unsigned_type meta_info_size = bytes % raw_size;
-        STXXL_VERBOSE1("typed::block operator new[]: bytes=" << bytes << ", meta_info_size=" << meta_info_size);
+        STXXL_VERBOSE_TYPED_BLOCK("typed::block operator new[]: bytes=" << bytes << ", meta_info_size=" << meta_info_size);
 
         void* result = aligned_alloc<STXXL_BLOCK_ALIGN>(
             bytes - meta_info_size, meta_info_size);
