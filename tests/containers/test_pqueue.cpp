8e8c9433d85bcaf1b5e0945aa79bee3a906e185e
Timo Bingmann <tbgit@panthema.net>
2014-11-03 13:07:59 +0000

    Cleaning up old PQ test test_pqueue.cpp.

--- tests/containers/test_pqueue.cpp
+++ tests/containers/test_pqueue.cpp
@@ -5,6 +5,7 @@
  *
  *  Copyright (C) 2003 Roman Dementiev <dementiev@mpi-sb.mpg.de>
  *  Copyright (C) 2009 Andreas Beckmann <beckmann@cs.uni-frankfurt.de>
+ *  Copyright (C) 2014 Timo Bingmann <tb@panthema.net>
  *
  *  Distributed under the Boost Software License, Version 1.0.
  *  (See accompanying file LICENSE_1_0.txt or copy at
@@ -19,7 +20,12 @@
 #include <stxxl/priority_queue>
 #include <stxxl/timer>
 
+using stxxl::uint64;
+using stxxl::scoped_print_timer;
+
 #define RECORD_SIZE 128
+
+const uint64 volume = 1024 * 1024; // in KiB
 
 struct my_type
 {
@@ -33,13 +39,12 @@ struct my_type
         memset(data, 0, sizeof(data));
 #endif
     }
-};
 
-std::ostream& operator << (std::ostream& o, const my_type& obj)
-{
-    o << obj.key;
-    return o;
-}
+    friend std::ostream& operator << (std::ostream& o, const my_type& obj)
+    {
+        return o << obj.key;
+    }
+};
 
 struct my_cmp : std::binary_function<my_type, my_type, bool> // greater
 {
@@ -55,23 +60,27 @@ struct my_cmp : std::binary_function<my_type, my_type, bool> // greater
 };
 
 // forced instantiation
-const unsigned volume = 1024 * 1024; // in KiB
-template class stxxl::PRIORITY_QUEUE_GENERATOR<my_type, my_cmp, 32* 1024* 1024, volume / sizeof(my_type)>;
+template class stxxl::PRIORITY_QUEUE_GENERATOR<
+    my_type, my_cmp, 32* 1024* 1024, volume / sizeof(my_type)
+    >;
 
 int main()
 {
 /*
-       unsigned BufferSize1_ = 32, // equalize procedure call overheads etc.
-       unsigned N_ = 512, // bandwidth
-       unsigned IntKMAX_ = 64, // maximal arity for internal mergers
-       unsigned IntLevels_ = 4,
-       unsigned BlockSize_ = (2*1024*1024),
-       unsigned ExtKMAX_ = 64, // maximal arity for external mergers
-       unsigned ExtLevels_ = 2,
+  unsigned BufferSize1_ = 32, // equalize procedure call overheads etc.
+  unsigned N_ = 512, // bandwidth
+  unsigned IntKMAX_ = 64, // maximal arity for internal mergers
+  unsigned IntLevels_ = 4,
+  unsigned BlockSize_ = (2*1024*1024),
+  unsigned ExtKMAX_ = 64, // maximal arity for external mergers
+  unsigned ExtLevels_ = 2,
  */
     //typedef priority_queue<priority_queue_config<my_type,my_cmp,
     //  32,512,64,3,(4*1024),0x7fffffff,1> > pq_type;
-    typedef stxxl::PRIORITY_QUEUE_GENERATOR<my_type, my_cmp, 32* 1024* 1024, volume / sizeof(my_type)> gen;
+
+    typedef stxxl::PRIORITY_QUEUE_GENERATOR<
+            my_type, my_cmp, 32* 1024* 1024, volume / sizeof(my_type)
+            > gen;
     typedef gen::result pq_type;
     typedef pq_type::block_type block_type;
 
@@ -81,24 +90,29 @@ int main()
     STXXL_MSG("N : " << gen::N);
     STXXL_MSG("AE: " << gen::AE);
 
-    stxxl::timer Timer;
-    Timer.start();
-
     const unsigned mem_for_pools = 128 * 1024 * 1024;
-    stxxl::read_write_pool<block_type> pool((mem_for_pools / 2) / block_type::raw_size, (mem_for_pools / 2) / block_type::raw_size);
+    stxxl::read_write_pool<block_type> pool(
+        (mem_for_pools / 2) / block_type::raw_size,
+        (mem_for_pools / 2) / block_type::raw_size
+        );
     pq_type p(pool);
 
-    stxxl::int64 nelements = stxxl::int64(volume * 1024 / sizeof(my_type)), i;
+    uint64 nelements = volume * 1024 / sizeof(my_type);
     STXXL_MSG("Internal memory consumption of the priority queue: " << p.mem_cons() << " B");
     STXXL_MSG("Max elements: " << nelements);
-    for (i = 0; i < nelements; i++)
+
     {
-        if ((i % (1024 * 1024)) == 0)
-            STXXL_MSG("Inserting element " << i);
-        p.push(my_type(int(nelements - i)));
+        scoped_print_timer timer("Filling PQ",
+                                 nelements * sizeof(my_type));
+
+        for (uint64 i = 0; i < nelements; i++)
+        {
+            if ((i % (1024 * 1024)) == 0)
+                STXXL_MSG("Inserting element " << i);
+            p.push(my_type(int(nelements - i)));
+        }
     }
-    Timer.stop();
-    STXXL_MSG("Time spent for filling: " << Timer.seconds() << " s");
+
     STXXL_CHECK(p.size() == (stxxl::uint64)nelements);
 
 #if 0
@@ -109,22 +123,26 @@ int main()
 #endif
 
     STXXL_MSG("Internal memory consumption of the priority queue: " << p.mem_cons() << " B");
-    Timer.reset();
-    Timer.start();
-    for (i = 0; i < (nelements); ++i)
+
     {
-        STXXL_CHECK(!p.empty());
-        //STXXL_MSG( p.top() );
-        STXXL_CHECK(p.top().key == i + 1);
-        p.pop();
-        if ((i % (1024 * 1024)) == 0)
-            STXXL_MSG("Element " << i << " popped");
+        scoped_print_timer timer("Emptying PPQ",
+                                 nelements * sizeof(my_type));
+
+        for (uint64 i = 0; i < nelements; ++i)
+        {
+            STXXL_CHECK(!p.empty());
+            //STXXL_MSG( p.top() );
+            STXXL_CHECK(p.top().key == int(i + 1));
+            p.pop();
+            if ((i % (1024 * 1024)) == 0)
+                STXXL_MSG("Element " << i << " popped");
+        }
     }
-    Timer.stop();
-    STXXL_MSG("Time spent for removing elements: " << Timer.seconds() << " s");
 
     STXXL_CHECK(p.size() == 0);
     STXXL_CHECK(p.empty());
 
     STXXL_MSG("Internal memory consumption of the priority queue: " << p.mem_cons() << " B");
+
+    return 0;
 }
