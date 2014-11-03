61216d9b9589ca43c9ae07917e188d5934455667
Timo Bingmann <tbgit@panthema.net>
2014-11-03 13:53:58 +0000

    Fixing warnings in test_winner_tree.cpp

--- tests/common/test_winner_tree.cpp
+++ tests/common/test_winner_tree.cpp
@@ -22,12 +22,13 @@ struct VectorCompare
     //! number of vectors in comparator
     size_t m_vecnum;
     //! vector of pointers to player streams
-    const std::vector<unsigned int>* m_vec;
+    const std::vector< std::vector<unsigned int> >& m_vec;
 
     //! currently top indices
     std::vector<unsigned int> m_vecp;
 
-    VectorCompare(size_t vecnum, const std::vector<unsigned int>* vec)
+    VectorCompare(size_t vecnum,
+                  const std::vector< std::vector<unsigned int> >& vec)
         : m_vecnum(vecnum), m_vec(vec), m_vecp(vecnum, 0)
     { }
 
@@ -51,7 +52,7 @@ void test_vecs(size_t vecnum)
     // construct many vectors of sorted random numbers
 
     stxxl::random_number32 rnd;
-    std::vector<unsigned int> vec[vecnum];
+    std::vector< std::vector<unsigned int> > vec (vecnum);
 
     size_t totalsize = 0;
     std::vector<unsigned int> output, correct;
@@ -59,7 +60,7 @@ void test_vecs(size_t vecnum)
     for (size_t i = 0; i < vecnum; ++i)
     {
         // determine number of items in stream
-        int inum = (rnd() % 128) + 64;
+        size_t inum = (rnd() % 128) + 64;
         vec[i].resize(inum);
         totalsize += inum;
 
