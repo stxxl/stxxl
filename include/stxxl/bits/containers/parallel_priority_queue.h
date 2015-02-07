78caf8b7ce397956d23f2678ad8ed8ce92fdc2f9
Timo Bingmann <tbgit@panthema.net>
2015-02-07 20:01:01 +0100

    PPQ: adding my copyright stamp.

--- include/stxxl/bits/containers/parallel_priority_queue.h
+++ include/stxxl/bits/containers/parallel_priority_queue.h
@@ -3,7 +3,8 @@
  *
  *  Part of the STXXL. See http://stxxl.sourceforge.net
  *
- *  Copyright (C) 2014 Thomas Keh <thomas.keh@student.kit.edu>
+ *  Copyright (C) 2014-2015 Thomas Keh <thomas.keh@student.kit.edu>
+ *  Copyright (C) 2014-2015 Timo Bingmann <tb@panthema.net>
  *
  *  Distributed under the Boost Software License, Version 1.0.
  *  (See accompanying file LICENSE_1_0.txt or copy at
