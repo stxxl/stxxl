d248a54c6803f0d1164d3ec9e7ac1db1bff0e293
Andreas Beckmann <a.beckmann@fz-juelich.de>
2017-02-14 12:29:13 +0100

    drop superfluous semicolons

--- include/stxxl/bits/common/types.h
+++ include/stxxl/bits/common/types.h
@@ -64,13 +64,13 @@ typedef int64_t  external_diff_type;         // may require external memory
 template <typename Integral>
 typename std::make_unsigned<Integral>::type as_unsigned(Integral value) {
     return static_cast<typename std::make_unsigned<Integral>::type>(value);
-};
+}
 
 //! Return the given value casted to the corresponding signed type
 template <typename Integral>
 typename std::make_signed<Integral>::type as_signed(Integral value) {
     return static_cast<typename std::make_signed<Integral>::type>(value);
-};
+}
 
 STXXL_END_NAMESPACE
 
