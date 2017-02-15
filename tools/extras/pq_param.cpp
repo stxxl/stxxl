ab6bdc490f45488f32447d6c82473181935a251f
Michael Hamann <michael.hamann@kit.edu>
2017-02-15 16:53:39 +0100

    Change pq_param.cpp to new integer types

--- tools/extras/pq_param.cpp
+++ tools/extras/pq_param.cpp
@@ -14,9 +14,7 @@
 #include <cmath>
 #include <stxxl/types>
 
-typedef stxxl::int64 int64;
-
-int64 D(int64 m, int64 k, int64 MaxS, int64 E, int64 B)
+int64_t D(int64_t m, int64_t k, int64_t MaxS, int64_t E, int64_t B)
 {
     return (m * m / 4 - (MaxS * E / (k - m)) / B);
 }
@@ -26,19 +24,19 @@ using std::endl;
 
 int main()
 {
-    int64 IntM = 128 * 1024 * 1024;
-    int64 E = 4;
-    int64 B = 8 * 1024 * 1024;
-    //int64 Bstep = 128 * 1024;
-    int64 MaxS = (int64(128) * int64(1024 * 1024 * 1024)) / E;
+    int64_t IntM = 128 * 1024 * 1024;
+    int64_t E = 4;
+    int64_t B = 8 * 1024 * 1024;
+    //int64_t Bstep = 128 * 1024;
+    int64_t MaxS = (int64_t(128) * int64_t(1024 * 1024 * 1024)) / E;
 
     for ( ; B > 4096; B = B / 2)
     {
-        int64 m = 1;
-        int64 k = IntM / B;
+        int64_t m = 1;
+        int64_t k = IntM / B;
         for ( ; m < k; ++m)
         {
-            int64 c = (k - m);
+            int64_t c = (k - m);
             //if( D(m,k,MaxS,E,B)>= 0 && c > 10)
             if ((k - m) * m * m * B / (E * 4) >= MaxS)
             {
@@ -48,18 +46,18 @@ int main()
                 cout << "B: " << B << endl;
                 cout << "c: " << c << endl;
                 cout << "k: " << k << endl;
-                int64 Ae = m / 2;
-                int64 Ae1 = Ae + int64(sqrt((double)D(m, k, MaxS, E, B)));
-                int64 Ae2 = Ae - int64(sqrt((double)D(m, k, MaxS, E, B)));
-                int64 x = c * B / E;
-                int64 N = x / 4096;
+                int64_t Ae = m / 2;
+                int64_t Ae1 = Ae + int64_t(sqrt((double)D(m, k, MaxS, E, B)));
+                int64_t Ae2 = Ae - int64_t(sqrt((double)D(m, k, MaxS, E, B)));
+                int64_t x = c * B / E;
+                int64_t N = x / 4096;
                 cout << "Ae : " << Ae << endl;
                 cout << "Ae1: " << Ae1 << endl;
                 cout << "Ae2: " << Ae2 << endl;
                 cout << "minAe :" << (MaxS / (x * Ae)) << endl;
                 cout << "x  : " << x << endl;
                 cout << "N  : " << N << endl;
-                int64 Cons = x * E + B * (m / 2) + MaxS * B / (x * (m / 2));
+                int64_t Cons = x * E + B * (m / 2) + MaxS * B / (x * (m / 2));
                 cout << "COns : " << Cons << endl;
                 return 0;
             }
