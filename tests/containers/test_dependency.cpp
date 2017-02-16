c790b2d6d0a8b378bcab2ccc405fea340e1e0377
Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
2017-02-16 12:32:33 +0100

    Test to find ostream and unspecified dependencies

---
+++ tests/containers/test_dependency.cpp
@@ -0,0 +1,104 @@
+/***************************************************************************
+ *  tests/containers/test_ext_merger.cpp
+ *
+ *  Part of the STXXL. See http://stxxl.sourceforge.net
+ *
+ *  Copyright (C) 2017 Manuel Penschuck <manuel@ae.cs.uni-frankfurt.de>
+ *
+ *  Distributed under the Boost Software License, Version 1.0.
+ *  (See accompanying file LICENSE_1_0.txt or copy at
+ *  http://www.boost.org/LICENSE_1_0.txt)
+ **************************************************************************/
+
+#include <iostream>
+
+#include <stxxl/priority_queue>
+#include <stxxl/vector>
+#include <stxxl/sorter>
+
+#include <stxxl/deque>
+#include <stxxl/map>
+#include <stxxl/queue>
+#include <stxxl/sequence>
+#include <stxxl/unordered_map>
+
+struct MinimalType {
+    size_t a;
+
+    MinimalType(size_t a = 0) : a(a) {
+
+    }
+};
+
+struct MinimalTypeWithEq : public MinimalType {
+    size_t a;
+
+    MinimalTypeWithEq(size_t a = 0) : a(a) {
+
+    }
+
+    bool operator==(const MinimalType o) const {
+        return a == o.a;
+    }
+};
+
+
+template <typename T>
+struct CompareLessWithMin {
+    bool operator() (const T& a, const T& b) const {
+        return a.a < b.a;
+    }
+    T min_value() const {return {0};}
+};
+
+template <typename T>
+struct CompareLessWithMax {
+    bool operator() (const T& a, const T& b) const {
+        return a.a < b.a;
+    }
+    T max_value() const {return {std::numeric_limits<size_t>::max()};}
+};
+
+template <typename T>
+struct CompareLessWithMinMax {
+    bool operator()(const T &a, const T &b) const {
+        return a.a < b.a;
+    }
+
+    T min_value() const { return {0}; }
+    T max_value() const { return {std::numeric_limits<size_t>::max()}; };
+};
+
+template <typename T>
+struct HashFunctor
+{
+    size_t operator () (const T& a) const {
+        return a.a;
+    }
+};
+
+template class stxxl::priority_queue<
+        stxxl::priority_queue_config<MinimalType, CompareLessWithMin<MinimalType>, 32ul, 256ul, 16ul, 2ul, 16384ul, 16ul, 2ul, stxxl::RC>
+>;
+
+template class stxxl::vector<MinimalType>;
+
+template class stxxl::sorter<MinimalType, CompareLessWithMinMax<MinimalType> >;
+
+template class stxxl::deque<MinimalType>;
+
+template class stxxl::map<MinimalTypeWithEq, MinimalType, CompareLessWithMax<MinimalTypeWithEq>, 1024, 1024>;
+
+template class stxxl::queue<MinimalType>;
+
+template class stxxl::sequence<MinimalType>;
+
+template class stxxl::unordered_map<
+        MinimalType, MinimalType, HashFunctor<MinimalType>, CompareLessWithMinMax<MinimalType>, 1024, 128
+>;
+
+
+int main() {
+    std::cout << "Nothing to see here" << std::endl;
+    return 0;
+}
